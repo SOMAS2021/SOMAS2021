4aabe8f0463f6886c1f1360be9e73521f63eb1b7
Matthew Scott <matthew.scott3142@gmail.com>
2021-12-03 19:36:03 +0000

    added basic behavioural update algorithm

---
+++ pkg/agents/team6/behaviours.go
@@ -0,0 +1,45 @@
+package team6
+
+import (
+	"math"
+)
+
+func (a *CustomAgent6) updateBehaviour() {
+
+	aConf := a.config
+	behaviourMax, behaviourMin := a.getBehaviourRange()
+
+	hpScore := 1 - float64(a.HP())/100.0 //map (minHP,maxHP) -> (1,0)
+	floor := a.Floor() + 1
+	floor = 600
+
+	if floor > a.maxFloorGuess {
+		a.maxFloorGuess = floor + 1
+	}
+
+	fdNum := float64(floor) / float64(a.maxFloorGuess) * aConf.lambda
+	fdDen := float64(aConf.lambda)
+	floorScore := math.Exp(fdNum) / math.Exp(fdDen)
+
+	behaviourParams := []float64{hpScore, floorScore}
+
+	weights := aConf.paramWeights
+	behaviourPrediction := 0.0
+
+	for i := range weights {
+		behaviourPrediction += behaviourParams[i] * weights[i]
+	}
+
+	updateDir := behaviour(behaviourPrediction) - a.currBehaviour //find direction we need to move in to reach new behaviour prediction
+	updateMag := updateDir * behaviour(1-aConf.stubbornness)      //scale movement by stubbornness (minStubborn, maxStubborn) -> (fullMovement, 0)
+	newBehaviour := a.currBehaviour + updateMag
+
+	if newBehaviour > behaviourMax { //limit behaviour to max swing
+		a.currBehaviour = behaviourMax
+	} else if newBehaviour < behaviourMin {
+		a.currBehaviour = behaviourMin
+	} else {
+		a.currBehaviour = newBehaviour
+	}
+
+}
--- pkg/agents/team6/config.go
+++ pkg/agents/team6/config.go
@@ -2,11 +2,13 @@ package team6
 
 import (
 	"log"
-  "math/rand"
+	"math"
+	"math/rand"
+
 	"github.com/SOMAS2021/SOMAS2021/pkg/agents"
 )
 
-type behaviour int
+type behaviour float64
 
 const (
 	altruist behaviour = iota
@@ -16,31 +18,63 @@ const (
 )
 
 type team6Config struct {
-  currBehaviour behaviour
-  stubborn float64
+	baseBehaviour behaviour
+	//the scaling factor which limits the change in agent behaviour
+	stubbornness float64
+	//the largest jump in behaviour an agent can take
+	maxBehaviourSwing float64
+	//weights used to assess score for behaviour update
+	paramWeights behaviourParameterWeights
+	//floor scaling discount factor
+	lambda float64
 }
 
 type CustomAgent6 struct {
 	*agents.Base
-  config team6Config
+	config team6Config
+	//keep track of the lowest floor we've been to
+	maxFloorGuess int
+	currBehaviour behaviour
 }
 
-func chooseInitialBehaviour() (behaviour){
-  return behaviour(rand.Intn(4))
+type behaviourParameterWeights []float64
+
+func chooseInitialBehaviour() behaviour {
+	return behaviour(rand.Intn(4))
 }
 
 func New(baseAgent *agents.Base) (agents.Agent, error) {
+	initialBehaviour := chooseInitialBehaviour()
 	return &CustomAgent6{
-		Base:     baseAgent,
-    config: team6Config{
-      currBehaviour: chooseInitialBehaviour(),
-  		stubborn:			0.5,
-    },
-
+		Base: baseAgent,
+		config: team6Config{
+			baseBehaviour:     initialBehaviour,
+			stubbornness:      0.5,
+			maxBehaviourSwing: 1,
+			paramWeights:      behaviourParameterWeights{2.0, 10.0},
+			lambda:            3.0,
+		},
+		currBehaviour: initialBehaviour,
+		maxFloorGuess: baseAgent.Floor() + 2,
 	}, nil
 }
 
 func (a *CustomAgent6) Run() {
 	log.Printf("Custom agent team 6 has floor: %d", a.Floor())
-	log.Printf("Custom agent team 6 has behaviour: %d", a.config.currBehaviour)
+	log.Printf("Team 6 has behaviour: %f", a.currBehaviour)
+	log.Printf("Team 6 has maxFloorGuess: %d", a.maxFloorGuess)
+	a.updateBehaviour()
+	log.Printf("Team 6 has behaviour: %f", a.currBehaviour)
+	log.Printf("Team 6 has maxFloorGuess: %d", a.maxFloorGuess)
+
+}
+
+func (a *CustomAgent6) getBehaviourRange() (behaviourMax, behaviourMin behaviour) {
+
+	aConf := a.config
+
+	bMax := behaviour(math.Min(3, float64(aConf.baseBehaviour)+aConf.maxBehaviourSwing))
+	bMin := behaviour(math.Max(0, float64(aConf.baseBehaviour)-aConf.maxBehaviourSwing))
+
+	return bMax, bMin
 }
