84bd836050042f9c19784e181640c7acff1f21e4
Silvertrousers <c.goodarzi@gmail.com>
2021-12-08 22:37:41 +0000

    added message types

---
+++ pkg/infra/messages/baseMessage.go
@@ -0,0 +1,29 @@
+package messages
+
+//Define message types to enable basic protocols, voting systems ...etc
+type Message interface {
+	MessageType() string
+}
+
+type baseMessage struct {
+	SenderFloor  uint
+	responseType Message
+}
+
+func NewBaseMessage(SenderFloor uint) *baseMessage {
+	msg := &baseMessage{
+		SenderFloor: SenderFloor,
+	}
+	return msg
+}
+func (msg *baseMessage) MessageType() string {
+	return "baseMessage"
+}
+
+// func (msg *baseMessage) SenderFloor() uint {
+// 	return msg.senderFloor
+// }
+
+func (msg *baseMessage) ResponseType() string {
+	return msg.responseType.MessageType()
+}
