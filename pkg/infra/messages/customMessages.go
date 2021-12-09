40b6d6a254855120acdb4d33f412073cc1ef98f0
Silvertrousers <c.goodarzi@gmail.com>
2021-12-09 12:11:55 +0000

    commented unfinished custom messages

--- pkg/infra/messages/customMessages.go
+++ pkg/infra/messages/customMessages.go
@@ -1,25 +1,25 @@
 package messages
 
 
-type IntMessage struct {
-	*baseMessage
-	i int
-}
-type YesNoMessage struct {
-	*baseMessage
-	YesNo bool
-}
+// type IntMessage struct {
+// 	*baseMessage
+// 	i int
+// }
+// type YesNoMessage struct {
+// 	*baseMessage
+// 	YesNo bool
+// }
 
-// voting system messages 
-// chaining messages
-//idea of this protocol is: 
-type FloorTargetChainMessage struct {
-	*baseMessage
-	TargetFloor uint64
-	//message content
-}
-type AgentTargetChainMessage struct {
-	*baseMessage
-	TargetAgentID string
-	//message content
-}
+// // voting system messages 
+// // chaining messages
+// //idea of this protocol is: 
+// type FloorTargetChainMessage struct {
+// 	*baseMessage
+// 	TargetFloor uint64
+// 	//message content
+// }
+// type AgentTargetChainMessage struct {
+// 	*baseMessage
+// 	TargetAgentID string
+// 	//message content
+// }
