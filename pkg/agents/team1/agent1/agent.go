0de3a59d41f46acc0222a5e6656ec2303a7183be
Silvertrousers <c.goodarzi@gmail.com>
2021-12-09 00:12:31 +0000

    added some agent mnessage sending behaviour

--- pkg/agents/team1/agent1/agent.go
+++ pkg/agents/team1/agent1/agent.go
@@ -21,8 +21,40 @@ func New(baseAgent *agents.Base) (abm.Agent, error) {
 }
 
 func (a *CustomAgent1) Run() {
-	log.Printf("Custom agent in team 1 is on floor %d has hp: %d", a.Floor(), a.HP())
-	a.TakeFood(10)
+	// log.Printf("Custom agent in team 1 is on floor %d has hp: %d", a.Floor(), a.HP())
+	// a.takeFood(10)
+	// a.HP()
+	// a.IsDead()
+
+	
+	if a.Base.Exists() {
+		log.Printf("Custom agent %s in team 1 has floor: %d", a.ID(), a.Floor())
+		msg := *messages.NewAckMessage(uint(a.Floor()), 3, true)
+		log.Printf("Custom agent team 1 has floor: %d", a.Floor())
+		if a.Floor() < 9 {a.Base.SendMessage(1, msg)}
+		// //make message
+		receivedMsg := a.ReceiveMessage()
+		if receivedMsg != nil {
+			log.Printf("Custom agent team 1 has floor: %d, I got a msg from: %d", a.Floor(), receivedMsg.MessageType())
+		}
+		a.TakeFood(1)
+		
+	} else {
+		log.Printf("Agent %s No longer exists", a.ID())
+	}
+	
+}
+
+func (a *CustomAgent1) HP() int {
+	return a.Base.HP()
+}
+
+func (a *CustomAgent1) IsDead() bool {
+	return a.Base.IsDead()
+}
+
+func (a *CustomAgent1) takeFood(foodToTake float64) float64 {
+	return a.Base.TakeFood(foodToTake)
 }
 
 func (a *CustomAgent1) AddToInbox(msg messages.Message) {
