b9f24ae6e89b5c676aaa068fc56048398a140039
Jason Zheng <jzz18@ic.ac.uk>
2021-12-10 17:11:21 +0000

    Generate logs in logs directory and latest log in simulation.log

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"time"
 
 	"github.com/SOMAS2021/SOMAS2021/pkg/simulation"
 	log "github.com/sirupsen/logrus"
@@ -10,13 +12,44 @@ import (
 
 func main() {
 	// logger setup
-	f, err := os.OpenFile("test.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
-	if err != nil {
-		fmt.Printf("error opening file: %v", err)
+	if _, err := os.Stat("logs"); os.IsNotExist(err) {
+		err := os.Mkdir("logs", 0755)
+		if err != nil {
+			fmt.Println("failed to create logs directory: ", err)
+			return
+		}
 	}
-	defer f.Close()
+
+	logfileName := "logs/" + time.Now().Format(time.RFC3339) + ".log"
+
+	f, err := os.OpenFile(logfileName, os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		fmt.Println("error opening file: ", err)
+		return
+	}
+	defer func() {
+		simLog, err := os.OpenFile("simulation.log", os.O_CREATE|os.O_RDWR, 0666)
+		if err != nil {
+			fmt.Println("error creating simulation log: ", err)
+			return
+		}
+		defer simLog.Close()
+		f, err = os.Open(logfileName)
+		if err != nil {
+			fmt.Println("error opening file: ", err)
+			return
+		}
+		defer f.Close()
+		_, err = io.Copy(simLog, f)
+		if err != nil {
+			fmt.Println("error copying to simulation log: ", err)
+			return
+		}
+	}()
+
 	log.SetOutput(f)
 	log.SetFormatter(&log.JSONFormatter{})
+
 	// can have frontend parameters come go straight into simEnv
 	foodOnPlatform := 100.0
 	numOfAgents := []int{5, 5}
