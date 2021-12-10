3dcd4a1033ec56ea3a489599bd21b0a29e2e4ed3
Jaafar Rammal <45538723+JaafarRammal@users.noreply.github.com>
2021-12-10 17:24:38 +0000

    Infra logging using logrus (#35)
    * logger implemented
    
    * linting
    
    * make json output + fix gitignore
    
    * Generate logs in logs directory and latest log in simulation.log
    
    * added comments for clarity
    
    * Apply suggestions from code review
    
    Co-authored-by: Jason Zheng <jzz18@ic.ac.uk>

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -1,15 +1,68 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+
 	"github.com/SOMAS2021/SOMAS2021/pkg/simulation"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	// logger setup
+	// TODO: clean up logger initialisation and closing code
+	if _, err := os.Stat("logs"); os.IsNotExist(err) {
+		err := os.Mkdir("logs", 0755)
+		if err != nil {
+			fmt.Println("failed to create logs directory: ", err)
+			return
+		}
+	}
+
+	// archive logs by default
+	logfileName := "logs/" + time.Now().Format(time.RFC3339) + ".log"
+
+	// open latest archive
+	f, err := os.OpenFile(logfileName, os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		fmt.Println("error opening file: ", err)
+		return
+	}
+
+	// copy latest archive to simulation.log
+	defer func() {
+		// open simulation.log
+		simLog, err := os.OpenFile("simulation.log", os.O_CREATE|os.O_RDWR, 0666)
+		if err != nil {
+			fmt.Println("error creating simulation log: ", err)
+			return
+		}
+		// close simulation.log
+		defer simLog.Close()
+		f, err = os.Open(logfileName)
+		if err != nil {
+			fmt.Println("error opening file: ", err)
+			return
+		}
+		// close latest archive
+		defer f.Close()
+		_, err = io.Copy(simLog, f)
+		if err != nil {
+			fmt.Println("error copying to simulation log: ", err)
+			return
+		}
+	}()
+
+	log.SetOutput(f)
+	log.SetFormatter(&log.JSONFormatter{})
+
 	// can have frontend parameters come go straight into simEnv
 	foodOnPlatform := 100.0
 	numOfAgents := []int{5, 5}
 	agentHP := 100
-	days := 10
+	days := 1
 	daysPerReshuffle := 3
 
 	iterationsPerDay := 24 * 60
