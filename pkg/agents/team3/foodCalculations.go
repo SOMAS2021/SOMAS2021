0ef1d7cc2adfa3c96b30c7e68db3b123d59ddab7
Eschamp01 <edwardjls@gmail.com>
2021-12-16 18:06:40 +0000

    Update foodCalculations.go

--- pkg/agents/team3/foodCalculations.go
+++ pkg/agents/team3/foodCalculations.go
@@ -1,1 +1,17 @@
 package team3
+
+import (
+	"math"
+)
+
+/*
+stubbornness range = 0-100
+mood range = 0-100
+morality range = 0-100
+*/
+
+func takeFoodCalculation(a *CustomAgent3) int64 {
+	//food taken is: 10 - moarlity/20 - mood/25
+	var try_take_food = 10.0 - math.Floor(float64(a.vars.morality/20.0)) - math.Floor(float64(a.vars.mood)/20.0)
+	return int64(try_take_food)
+}
