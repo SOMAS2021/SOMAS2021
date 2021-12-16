5f3e59a4228c9ee1b9d03d71d12b2e65032c99e5
Rhys-J <rhysjohnson16@gmail.com>
2021-12-16 15:19:40 +0000

    Adding team 5 agent to simulation

--- pkg/agents/team5/agent1/agent.go
+++ pkg/agents/team5/agent1/agent.go
@@ -1,42 +1,22 @@
-package agent1
+package team5
 
 import (
 	"github.com/SOMAS2021/SOMAS2021/pkg/infra"
-	"github.com/SOMAS2021/SOMAS2021/pkg/messages"
 	"github.com/SOMAS2021/SOMAS2021/pkg/utils/abm"
 )
 
 type CustomAgent1 struct {
 	*infra.Base
-	myNumber int
 }
 
 func New(baseAgent *infra.Base) (abm.Agent, error) {
 	return &CustomAgent1{
-		Base:     baseAgent,
-		myNumber: 0,
+		Base: baseAgent,
 	}, nil
 }
 
 func (a *CustomAgent1) Run() {
 	a.Log("Reporting agent state", infra.Fields{"health": a.HP(), "floor": a.Floor()})
 
-	receivedMsg := a.Base.ReceiveMessage()
-	if receivedMsg != nil {
-		a.Log("I sent a message", infra.Fields{"message": receivedMsg.MessageType()})
-	} else {
-		a.Log("I got nothing")
-	}
-
-	if (a.myNumber)%2 == 0 {
-		msg := *messages.NewAckMessage(int(a.Floor()), true)
-		a.SendMessage(1, msg)
-		a.Log("I sent a message", infra.Fields{"message": msg.MessageType()})
-	} else {
-		msg := *messages.NewBaseMessage(int(a.Floor()))
-		a.SendMessage(1, msg)
-	}
-	a.Log("My agent is doing something", infra.Fields{"thing": "asdfasdf", "another_thing": "pepehands"})
-
-	a.TakeFood(10)
+	a.TakeFood(20)
 }
