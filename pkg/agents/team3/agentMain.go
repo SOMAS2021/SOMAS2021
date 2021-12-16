9aecb1ebbf39be5a965604aab78325fd3b811a62
fds18 <fds18@imperial.ac.uk>
2021-12-16 16:28:11 +0100

    Update agentMain.go

--- pkg/agents/team3/agentMain.go
+++ pkg/agents/team3/agentMain.go
@@ -2,8 +2,12 @@ package team3
 
 import (
 	"math/rand"
+<<<<<<< Updated upstream
 	"time"
 
+=======
+	"fmt"
+>>>>>>> Stashed changes
 	"github.com/SOMAS2021/SOMAS2021/pkg/infra"
 	//"github.com/SOMAS2021/SOMAS2021/pkg/messages"
 	"github.com/SOMAS2021/SOMAS2021/pkg/utils/abm"
@@ -18,10 +22,15 @@ type team3Variables struct {
 	mood int
 }
 
+type team3Knowledge struct {
+	//We know the floors we have been in
+	floors []int{}
+}
+
 type CustomAgent3 struct {
 	*infra.Base
 	vars team3Variables
-	//for later: add a array of ints to remember floors
+	knowledge team3Knowledge
 	//and an array of tuples for friendships
 }
 
@@ -39,15 +48,13 @@ func New(baseAgent *infra.Base) (abm.Agent, error) {
 }
 
 func (a *CustomAgent3) Run() {
-	a.Log("Reporting agent state", infra.Fields{"health": a.HP(), "floor": a.Floor()})
 
-	receivedMsg := a.Base.ReceiveMessage()
-	if receivedMsg != nil {
-		a.Log("I sent a message", infra.Fields{"message": receivedMsg.MessageType()})
-	} else {
-		a.Log("I got nothing")
-	}
+	//receive Message 
 
-	a.Log("My agent is doing something", infra.Fields{"thing": "potatoe", "another_thing": "another potatoe"})
-	a.TakeFood(10)
+	//emotion changes 
+
+	//eat
+
+	//send Message
+
 }
