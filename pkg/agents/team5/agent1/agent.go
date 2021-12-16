87698f8ef0c164344ffcf0d97e4382b487a47b55
Rhys-J <rhysjohnson16@gmail.com>
2021-12-16 16:31:26 +0000

    Adding desperation and selfishness to team 5 agent
    Added parameters to the custom agent 5 that vary and are used to determine how much food to take within the tower

--- pkg/agents/team5/agent1/agent.go
+++ pkg/agents/team5/agent1/agent.go
@@ -1,22 +1,61 @@
 package team5
 
 import (
+	"math"
+
 	"github.com/SOMAS2021/SOMAS2021/pkg/infra"
 	"github.com/SOMAS2021/SOMAS2021/pkg/utils/abm"
 )
 
-type CustomAgent1 struct {
+type CustomAgent5 struct {
 	*infra.Base
+	desperation  float64
+	selflishness float64
+	lastMeal     float64
 }
 
 func New(baseAgent *infra.Base) (abm.Agent, error) {
-	return &CustomAgent1{
-		Base: baseAgent,
+	return &CustomAgent5{
+		Base:         baseAgent,
+		desperation:  3.0, //Scale of 1 to 4, with 1 being near max health, 4 being about to die and 1 & 2 in between
+		selflishness: 2.0, // of 0 to 3, with 3 being completely selflish, 0 being completely selfless
+		lastMeal:     0,   //Stores value of the last amount of food taken
 	}, nil
 }
 
-func (a *CustomAgent1) Run() {
-	a.Log("Reporting agent state", infra.Fields{"health": a.HP(), "floor": a.Floor()})
+func (a *CustomAgent5) UpdateDesperation() {
+	switch {
+	case a.HP() <= 20:
+		a.desperation = 4.0
+	case a.HP() <= 50:
+		a.desperation = 3.0
+	case a.HP() < 80:
+		a.desperation = 2.0
+	case a.HP() >= 80:
+		a.desperation = 1.0
+	}
+}
 
-	a.TakeFood(20)
+func (a *CustomAgent5) UpdateSelfishness() {
+	//Some function that returns a new value of selfishness based on social network
+	//Will also be based on the desperation value
+}
+
+func (a *CustomAgent5) FoodAmount() float64 {
+	provisional := 20.0 * a.desperation //This determines the base level of food based on desperation
+	selfWeight := a.selflishness / 3.0  //This scales the taken amound by selfishness of agent
+	return math.Round(provisional * selfWeight)
+}
+
+func (a *CustomAgent5) Run() {
+	a.Log("Reporting agent state of team 5 agent", infra.Fields{"health": a.HP(), "floor": a.Floor()})
+	a.UpdateDesperation()
+	a.UpdateSelfishness()
+	attemptFood := a.FoodAmount()
+	if !a.HasEaten() {
+		a.lastMeal = a.TakeFood(attemptFood)
+		if a.lastMeal > 0 {
+			a.Log("Team 5 agent has taken food", infra.Fields{"amount": a.lastMeal})
+		}
+	}
 }
