67e9c8f8c989608d4d82af5eec1082d7dd71e058
SaraFFdez <77176781+SaraFFdez@users.noreply.github.com>
2021-12-16 14:53:16 +0000

    Added agent specification

--- pkg/agents/team3/agentMain.go
+++ pkg/agents/team3/agentMain.go
@@ -1,24 +1,41 @@
 package team3
 
 import (
+	"math/rand"
+
 	"github.com/SOMAS2021/SOMAS2021/pkg/infra"
-	"github.com/SOMAS2021/SOMAS2021/pkg/messages"
+	//"github.com/SOMAS2021/SOMAS2021/pkg/messages"
 	"github.com/SOMAS2021/SOMAS2021/pkg/utils/abm"
 )
 
-type CustomAgent1 struct {
+type team3Variables struct {
+	//Stubbornnes defines the likelyhood of reading a message
+	stubbornness int
+	//The willigness to help others/ how much you care
+	morale int
+	//A more volatile parameter that affects the decision making
+	mood int
+}
+
+type CustomAgent3 struct {
 	*infra.Base
-	myNumber int
+	vars team3Variables
+	//for later: add a array of ints to remember floors
+	//and an array of tuples for friendships
 }
 
 func New(baseAgent *infra.Base) (abm.Agent, error) {
-	return &CustomAgent1{
-		Base:     baseAgent,
-		myNumber: 0,
+	return &CustomAgent3{
+		Base: baseAgent,
+		vars: team3Variables{
+			stubbornness: rand.Intn(75),
+			morale:       rand.Intn(100),
+			mood:         rand.Intn(100),
+		},
 	}, nil
 }
 
-func (a *CustomAgent1) Run() {
+func (a *CustomAgent3) Run() {
 	a.Log("Reporting agent state", infra.Fields{"health": a.HP(), "floor": a.Floor()})
 
 	receivedMsg := a.Base.ReceiveMessage()
@@ -28,14 +45,6 @@ func (a *CustomAgent1) Run() {
 		a.Log("I got nothing")
 	}
 
-	if (a.myNumber)%2 == 0 {
-		msg := *messages.NewAckMessage(int(a.Floor()), true)
-		a.SendMessage(1, msg)
-		a.Log("I sent a message", infra.Fields{"message": msg.MessageType()})
-	} else {
-		msg := *messages.NewBaseMessage(int(a.Floor()))
-		a.SendMessage(1, msg)
-	}
 	a.Log("My agent is doing something", infra.Fields{"thing": "potatoe", "another_thing": "another potatoe"})
 	a.TakeFood(10)
 }
