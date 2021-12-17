9fb32f8168c5dd4dfbdd15f910f63327646752d6
mdubied <mdubied@ethz.ch>
2021-12-17 09:42:59 +0000

    Create health.go
    Create a HealthInfo struct and a NewHealthInfo function on the same model as DayInfo and NewDayInfo

---
+++ pkg/utils/globalTypes/health/health.go
@@ -0,0 +1,33 @@
+package health
+
+type HealthInfo struct {
+	StrongLevel     int // defined as the lowest HP value for a given HP level
+	HealthyLevel    int
+	WeakLevel       int
+	CriticalLevel   int
+	FoodReqStrong   int // food requirement to stay at a given level
+	FoodReqHealthy  int
+	FoodReqWeak     int
+	FoodReqCritical int
+	FoodReqHToS     int // food requirement to switch from healthy to strong
+	FoodReqWToH     int // food requirement to switch from weak to healthy
+	FoodReqCToW     int // food requirement to switch from critical to weak
+	MaxDayCritical  int // maximum number of day at critical level
+}
+
+func NewHealthInfo(StrongLevel, HealthyLevel, WeakLevel, CriticalLevel, FoodReqStrong, FoodReqHealthy, FoodReqWeak, FoodReqCrtical, MaxDayCritical, FoodReqHToS, FoodReqWToH, FoodReqCToW int) *HealthInfo {
+	return &HealthInfo{
+		StrongLevel:     StrongLevel,
+		HealthyLevel:    HealthyLevel,
+		WeakLevel:       WeakLevel,
+		CriticalLevel:   CriticalLevel,
+		FoodReqStrong:   FoodReqStrong,
+		FoodReqHealthy:  FoodReqHealthy,
+		FoodReqWeak:     FoodReqWeak,
+		FoodReqCritical: FoodReqCrtical,
+		MaxDayCritical:  MaxDayCritical,
+		FoodReqHToS:     FoodReqHToS,
+		FoodReqWToH:     FoodReqWToH,
+		FoodReqCToW:     FoodReqCToW,
+	}
+}
