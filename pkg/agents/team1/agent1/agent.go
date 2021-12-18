59b1079456792f99ed89a5ab2e3cbc716dddc314
mdubied <mdubied@ethz.ch>
2021-12-18 21:52:44 +0000

    agent.go (agent1) back with messages

--- pkg/agents/team1/agent1/agent.go
+++ pkg/agents/team1/agent1/agent.go
@@ -2,6 +2,7 @@ package agent1
 
 import (
 	"github.com/SOMAS2021/SOMAS2021/pkg/infra"
+	"github.com/SOMAS2021/SOMAS2021/pkg/messages"
 	"github.com/SOMAS2021/SOMAS2021/pkg/utils/globalTypes/agent"
 )
 
@@ -20,21 +21,21 @@ func New(baseAgent *infra.Base) (agent.Agent, error) {
 func (a *CustomAgent1) Run() {
 	a.Log("Reporting agent state", infra.Fields{"health": a.HP(), "floor": a.Floor()})
 
-	// receivedMsg := a.Base.ReceiveMessage()
-	// if receivedMsg != nil {
-	// 	a.Log("I sent a message", infra.Fields{"message": receivedMsg.MessageType()})
-	// } else {
-	// 	a.Log("I got nothing")
-	// }
+	receivedMsg := a.Base.ReceiveMessage()
+	if receivedMsg != nil {
+		a.Log("I sent a message", infra.Fields{"message": receivedMsg.MessageType()})
+	} else {
+		a.Log("I got nothing")
+	}
 
-	// if (a.myNumber)%2 == 0 {
-	// 	msg := *messages.NewAckMessage(int(a.Floor()), true)
-	// 	a.SendMessage(1, msg)
-	// 	a.Log("I sent a message", infra.Fields{"message": msg.MessageType()})
-	// } else {
-	// 	msg := *messages.NewBaseMessage(int(a.Floor()))
-	// 	a.SendMessage(1, msg)
-	// }
-	// a.Log("My agent is doing something", infra.Fields{"thing": "potatoe", "another_thing": "another potatoe"})
+	if (a.myNumber)%2 == 0 {
+		msg := *messages.NewAckMessage(int(a.Floor()), true)
+		a.SendMessage(1, msg)
+		a.Log("I sent a message", infra.Fields{"message": msg.MessageType()})
+	} else {
+		msg := *messages.NewBaseMessage(int(a.Floor()))
+		a.SendMessage(1, msg)
+	}
+	a.Log("My agent is doing something", infra.Fields{"thing": "potatoe", "another_thing": "another potatoe"})
 	a.TakeFood(16)
 }
