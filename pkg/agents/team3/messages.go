72e2cde9dbc7c0a18e2f96c3aa7e33de253fdc6b
eoinquig <eq3118@ic.ac.uk>
2021-12-20 14:58:12 +0000

    Message definitions, pseudocode.
    Early discussion of message and interactions with agent characteristics.

--- pkg/agents/team3/messages.go
+++ pkg/agents/team3/messages.go
@@ -1,1 +1,8 @@
 package team3
+//Upon receipt of message define affected emotions
+// ACK MESSAGE
+//If x time passed no message received/acked morale decrease
+//Include if ack message same user ID occurs x+1 times, morale increase
+//If stubborness = y+1, discard, a.k.a. leave unread
+
+//
