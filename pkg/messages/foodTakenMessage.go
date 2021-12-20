fc2b77ac093f60050852597beb7241e519d88e70
eoinquig <eq3118@ic.ac.uk>
2021-12-20 14:58:12 +0000

    Message definitions, pseudocode.
    Early discussion of message and interactions with agent characteristics.

--- pkg/messages/foodTakenMessage.go
+++ pkg/messages/foodTakenMessage.go
@@ -2,7 +2,7 @@ package messages
 
 type foodTakenMessage struct{
 	*baseMessage
-	FoodTaken float64 // Planning to change this to int, see #21 
+	FoodTaken float64 // Planning to change this to int, see #21
 }
 
 func NewfoodTakenMessage(SenderFloor int, foodTaken float64) *foodTakenMessage {
@@ -15,4 +15,13 @@ func NewfoodTakenMessage(SenderFloor int, foodTaken float64) *foodTakenMessage {
 
 func (msg foodTakenMessage) MessageType() string {
 	return "foodTakenMessage"
+
 }
+
+
+//return foodeaten.vari., ackmessage.
+//upon receipt of foodtaken, if foodtaken.int>your.foodtaken.int, morale decrease, stubborness increase.
+//if agent1 sends askFoodTakenMessage, agent2 sends foodTakenMessage.
+//then agent1 has morale inc/dec, mood inc/dec, stubborness inc/dec, if agent1 foodeaten<foodtaken agent2
+//agent2  has morale inc/dec, if agent1[userID] un/known. "fuck this guy".
+//
