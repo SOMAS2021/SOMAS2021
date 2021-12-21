ec8ca7107670e136ede99d236e6c8ef7ef375d3c
JeetendraJoshi <jeetendrajoshi23@gmail.com>
2021-12-21 15:14:14 +0000

    Added Global Trust + Plat Food Messages

--- pkg/agents/team4/agent2/agent.go
+++ pkg/agents/team4/agent2/agent.go
@@ -9,19 +9,49 @@ import (
 type CustomAgent4 struct {
 	*infra.Base
 	myNumber int
+	globalTrust int
 }
 
 func New(baseAgent *infra.Base) (agent.Agent, error) {
 	return &CustomAgent4{
 		Base:     baseAgent,
 		myNumber: 0,
+		globalTrust: 0, // MAKE SURE TO AMEND FIGURES FOR SENSIBLE AGENT BEHAVIOUR
+		globalTicks: 0,
 	}, nil
 }
 
 func (a *CustomAgent4) Run() {
 	a.Log("Reporting agent state", infra.Fields{"health": a.HP(), "floor": a.Floor()})
 
-	receivedMsg := a.Base.ReceiveMessage()
+	defer a.globalTicks++ // count number of passed ticks/ days passed
+	if globalTicks %2 ==0 && globalTicks !=0{
+		receivedMsg := a.Base.ReceiveMessage()
+		switch receivedMsg.MessageType() {
+		case "AckMessage":
+			a.globalTrust +=1 // TODO AND WORK ON
+		// case "foodOnPlatMessage":
+		// 	if receivedMsg.food == a.CurrPlatFood() && a.CurrPlatFood() != -1
+	  case "LeaveFoodMessage":
+			if receivedMsg.food == a.currPlatFood() && receivedMsg.senderFloor == -1 && a.CurrPlatFood() != -1{
+				globalTrust+= 9 //
+
+			} else if receivedMsg.food != a.currPlatFood() && receivedMsg.senderFloor == -1 && a.CurrPlatFood() != -1{
+
+			}
+
+
+		default:
+		}
+
+
+	}
+
+
+	// switch receivedMsg.MessageType() {
+	// case condition:
+	//
+	// }
 	if receivedMsg != nil {
 		a.Log("I sent a message", infra.Fields{"message": receivedMsg.MessageType()})
 	} else {
