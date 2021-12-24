00bcee360e2996eabe2ad31bdb7113c846b52657
moinbukhari <smb18@ic.ac.uk>
2021-12-24 15:27:31 +0000

    basic agent

---
+++ pkg/agents/team7/agent2/agent.go
@@ -0,0 +1,27 @@
+package agent2
+
+import (
+	"log"
+
+	"github.com/SOMAS2021/SOMAS2021/pkg/infra"
+	"github.com/SOMAS2021/SOMAS2021/pkg/utils/abm"
+)
+
+type CustomAgent2 struct {
+	*infra.Base
+	myNumber int
+	// new params
+}
+
+func New(baseAgent *infra.Base) (abm.Agent, error) {
+	//create other parameters
+	return &CustomAgent2{
+		Base:     baseAgent,
+		myNumber: 0,
+	}, nil
+}
+
+func (a *CustomAgent2) Run() {
+	log.Printf("Custom agent in team 7 is on floor %d has hp: %d", a.Floor(), a.HP())
+	a.TakeFood(100)
+}
