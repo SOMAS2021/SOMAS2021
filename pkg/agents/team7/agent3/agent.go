2ad3ad7c76635d92f6cc5ba89d882ee7131cd8d4
Nasim M <37720652+NasimMM@users.noreply.github.com>
2021-12-24 16:27:48 +0000

    Added Greedy Agent

---
+++ pkg/agents/team7/agent3/agent.go
@@ -0,0 +1,37 @@
+package team7
+
+import (
+	"github.com/SOMAS2021/SOMAS2021/pkg/infra"
+	"github.com/SOMAS2021/SOMAS2021/pkg/utils/globalTypes/agent"
+	"github.com/SOMAS2021/SOMAS2021/pkg/utils/globalTypes/food"
+)
+
+type CustomAgent1 struct {
+	*infra.Base
+	// new params
+}
+
+func New(baseAgent *infra.Base) (agent.Agent, error) {
+	//create other parameters
+	return &CustomAgent1{
+		Base: baseAgent,
+	}, nil
+}
+
+func (a *CustomAgent1) Run() {
+	a.Log("Agent7 reporting status:", infra.Fields{"floor": a.Floor(), "hp": a.HP()})
+
+	currentHP := a.HP()
+	//currentFloor := a.Floor()
+	//UserID := a.ID()
+	//currentAvailFood := a.CurrPlatFood()
+
+	var foodtotake food.FoodType = food.FoodType(100 - currentHP)
+	// var food food.FoodType = food.FoodType(foodtotake)
+	if foodtotake == 0 {
+
+	} else {
+		a.TakeFood(foodtotake)
+	}
+
+}
