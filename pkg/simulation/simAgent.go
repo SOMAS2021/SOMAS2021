b568e0ff303ad4d489cec14a950d640f762ed2cf
Jason Zheng <jzz18@ic.ac.uk>
2021-12-24 17:45:11 +0000

    Add Team 5 MVP (#106)
    * Add Team 5 initial agent
    
    * Update logging functions
    
    * Adding team 5 agent to simulation
    
    * Adding desperation and selfishness to team 5 agent
    
    Adding desperation and selfishness to team 5 agent
    
    Added parameters to the custom agent 5 that vary and are used to determine how much food to take within the tower
    
    * Integrating agent 5 with new infra
    
    Small change to fit agent 5 in with new infra set up
    
    * Minor comment changes
    
    * message interface functions
    
    added separate functions to process all messages in the inbox and to send all the messages we want
    
    * Update agent.go
    
    changed name for the receiving all that's in inbox
    
    * Team5 mvp memory (#76)
    
    * added memory to agent type
    
    * fixed type and added comment
    
    * added separate type for memory
    
    * Added NewMemory function
    
    Needed to pass linter test
    
    Co-authored-by: Rhys-J <rhysjohnson16@gmail.com>
    
    * Changes needed for new health system
    
    -Removed desperation and replaced with aim
    -Added updateSatisfaction
    -Added code to updateSelfishness
    -Added check for day passing
    -Added DayPassed()
    
    * Removing redundant variables in agent struct
    
    * Add comments and todos
    
    Co-authored-by: Rhys-J <rhysjohnson16@gmail.com>
    Co-authored-by: UMust99 <jjm4618@ic.ac.uk>
    Co-authored-by: Osman <59795625+tchiko7@users.noreply.github.com>

--- pkg/simulation/simAgent.go
+++ pkg/simulation/simAgent.go
@@ -5,6 +5,7 @@ import (
 	"github.com/SOMAS2021/SOMAS2021/pkg/agents/team1/agent1"
 	"github.com/SOMAS2021/SOMAS2021/pkg/agents/team1/agent2"
 	"github.com/SOMAS2021/SOMAS2021/pkg/agents/team3"
+	team5 "github.com/SOMAS2021/SOMAS2021/pkg/agents/team5/agent1"
 	"github.com/SOMAS2021/SOMAS2021/pkg/agents/team6"
 	"github.com/SOMAS2021/SOMAS2021/pkg/infra"
 	"github.com/google/uuid"
@@ -26,7 +27,7 @@ func (sE *SimEnv) generateInitialAgents(t *infra.Tower) {
 
 func (sE *SimEnv) createNewAgent(tower *infra.Tower, i, floor int) {
 	sE.Log("Creating new agent")
-	abs := []AgentNewFunc{agent1.New, agent2.New, team3.New, team6.New, randomAgent.New}
+	abs := []AgentNewFunc{agent1.New, agent2.New, team3.New, team5.New, team6.New, randomAgent.New}
 	uuid := uuid.New().String()
 
 	bAgent, err := infra.NewBaseAgent(sE.world, i, sE.AgentHP, floor, uuid)
