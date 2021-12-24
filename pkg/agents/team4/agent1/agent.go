0bd886b952a973e118a9a83669bccac80513467a
hussain2603 <56412306+hussain2603@users.noreply.github.com>
2021-12-24 23:21:22 +0400

    Team4 MVP (#111)
    * team4 agent mvp
    
    * Created new folder for team 4 evo custom agent
    
    * Created a system for optimising an agent using an evolutionn evevolutionary algorithm
    
    * Changed evolutionary algorithm to have the optimisation metric of everyones survival
    
    * Changed number of iterations fo evolutionary algorithm to 20
    
    * pushing error
    
    * Scaled the equations in team 4 evo agent
    
    * Changed mutation parameters in team4 evolutionary algorithm training
    
    * bug: changes to read in agent.go
    
    * Evo agent MVP
    
    * Update pkg/agents/team4/agent1/agent.go
    
    Co-authored-by: Jason Zheng <jzz18@ic.ac.uk>
    
    * Update agent.go
    
    * Update pkg/agents/team4/agent1/bestAgents.json
    
    Co-authored-by: Jason Zheng <jzz18@ic.ac.uk>
    
    * Update agent.go
    
    * Update pkg/agents/team4/agent1/agentConfig.json
    
    Co-authored-by: Jason Zheng <jzz18@ic.ac.uk>
    
    Co-authored-by: TeDand <tejas-d@hotmail.com>
    Co-authored-by: Jason Keung <jk4818@ic.ac.uk>
    Co-authored-by: adityag16 <aditya.gupta18@imperial.ac.uk>
    Co-authored-by: Jason Zheng <jzz18@ic.ac.uk>

---
+++ pkg/agents/team4/agent1/agent.go
@@ -0,0 +1,105 @@
+package team4EvoAgent
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"math/rand"
+	"os"
+
+	"github.com/SOMAS2021/SOMAS2021/pkg/infra"
+	"github.com/SOMAS2021/SOMAS2021/pkg/utils/globalTypes/food"
+)
+
+type Coefficient struct {
+	Floor []float64
+	Hp    []float64
+}
+
+type Equation struct {
+	coefficients []float64
+}
+
+// function to evaluate a function f(x) at a specific value of x0
+func (equation *Equation) EvaluateEquation(input int) float64 {
+	sum := 0.0
+	for i, coeff := range equation.coefficients {
+		power := math.Pow(float64(input), float64(i))
+		sum += coeff * power
+	}
+	return sum
+}
+
+// function to pick the correct co-efficients from the configuration file
+func GenerateEquations() (Equation, Equation) {
+	mydir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+	file, _ := ioutil.ReadFile(fmt.Sprintf("%s/pkg/agents/team4/agent1/agentConfig.json", mydir))
+	var data1 Coefficient
+	_ = json.Unmarshal(file, &data1) //parse the config json file to get the coeffs for floor and HP equations
+
+	var newAgentFloor Equation
+	var newAgentHp Equation
+
+	newAgentFloor.coefficients = data1.Floor
+	newAgentHp.coefficients = data1.Hp
+
+	return newAgentFloor, newAgentHp
+}
+
+type CustomAgentEvoParams struct {
+	// these params are based only on singular inputs
+	scalingEquation   Equation
+	currentFloorScore Equation
+	currentHpScore    Equation
+	// below params updated based in previous experience of floors
+	trustscore float64
+	morality   float64
+	trauma     float64
+}
+
+type CustomAgentEvo struct {
+	*infra.Base
+	// new params
+	params CustomAgentEvoParams
+}
+
+func New(baseAgent *infra.Base) (infra.Agent, error) {
+	currentFloorScoreEquation, currentHpScoreEquation := GenerateEquations()
+	//create other parameters
+	return &CustomAgentEvo{
+		Base: baseAgent,
+		params: CustomAgentEvoParams{ //initialise the parameters of the agent
+			scalingEquation: Equation{
+				coefficients: []float64{1, 1, 1, 1},
+			},
+			currentFloorScore: currentFloorScoreEquation,
+			currentHpScore:    currentHpScoreEquation,
+			trustscore:        100 * rand.Float64(), //random for now
+			morality:          100 * rand.Float64(), //random for now
+			trauma:            0,                    //0 for now
+		},
+	}, nil
+}
+
+func (a *CustomAgentEvo) Run() {
+
+	// Using custom params to decide how much food to eat
+	scaledFloorScore := a.params.currentFloorScore.EvaluateEquation(a.Floor()) / a.params.scalingEquation.EvaluateEquation(a.Floor())
+	scaledHpScore := a.params.currentHpScore.EvaluateEquation(a.HP()) / a.params.scalingEquation.EvaluateEquation(a.HP())
+	foodToEat := food.FoodType(math.Max(0.0, 50*scaledFloorScore+50*scaledHpScore))
+
+	foodEaten := a.TakeFood(foodToEat)
+
+	a.Log("team4EvoAgent reporting status:", infra.Fields{"floor": a.Floor(), "hp": a.HP(), "foodToEat": foodToEat, "foodEaten": foodEaten, "currentFloorScore": a.params.currentFloorScore.coefficients, "currentHpScore": a.params.currentHpScore.coefficients})
+
+	// Trauma score for future use
+	if a.HP() < 25 {
+		a.params.trauma = math.Min(100, a.params.trauma+1)
+	} else if a.HP() > 75 {
+		a.params.trauma = math.Max(0, a.params.trauma-1)
+	}
+}
