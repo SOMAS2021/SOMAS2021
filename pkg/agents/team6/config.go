e714d956a4455389ae06d6724c1c91ae5bcda81e
Matt Scott <72709100+MattScottEEE@users.noreply.github.com>
2021-12-24 16:34:31 +0000

    Team6 Food Intake Strategy Update (#95)

--- pkg/agents/team6/config.go
+++ pkg/agents/team6/config.go
@@ -5,7 +5,6 @@ import (
 	"math/rand"
 
 	"github.com/SOMAS2021/SOMAS2021/pkg/infra"
-	"github.com/SOMAS2021/SOMAS2021/pkg/messages"
 	"github.com/SOMAS2021/SOMAS2021/pkg/utils/globalTypes/agent"
 )
 
@@ -38,6 +37,7 @@ type CustomAgent6 struct {
 	//keep track of the lowest floor we've been to
 	maxFloorGuess int
 	currBehaviour behaviour
+	foodTakeDay   int
 }
 
 type thresholdBehaviourPair struct {
@@ -67,6 +67,7 @@ func New(baseAgent *infra.Base) (agent.Agent, error) {
 		},
 		currBehaviour: initialBehaviour,
 		maxFloorGuess: baseAgent.Floor() + 2,
+		foodTakeDay:   0,
 	}, nil
 }
 
@@ -85,18 +86,19 @@ func (b behaviour) String() string {
 }
 
 func (a *CustomAgent6) Run() {
-	a.Log("Custom agent 6 before update:", infra.Fields{"floor": a.Floor(), "hp": a.HP(), "behaviour": a.currBehaviour.String(), "maxFloorGuess": a.maxFloorGuess})
+	// a.Log("Custom agent 6 before update:", infra.Fields{"floor": a.Floor(), "hp": a.HP(), "behaviour": a.currBehaviour.String(), "maxFloorGuess": a.maxFloorGuess})
 
 	a.updateBehaviour()
 
-	a.Log("Custom agent 6 after update:", infra.Fields{"floor": a.Floor(), "hp": a.HP(), "behaviour": a.currBehaviour.String(), "maxFloorGuess": a.maxFloorGuess})
+	// a.Log("Custom agent 6 after update:", infra.Fields{"floor": a.Floor(), "hp": a.HP(), "behaviour": a.currBehaviour.String(), "maxFloorGuess": a.maxFloorGuess})
 
 	foodAmount := a.foodIntake()
 	a.TakeFood(foodAmount)
-	a.Log("Team 6 took:", infra.Fields{"foodTaken": foodAmount})
+	a.Log("Team 6 took:", infra.Fields{"foodTaken": foodAmount, "bType": a.currBehaviour.String()})
+	a.Log("Team 6 agent has HP:", infra.Fields{"HP": a.HP()})
 
-	msg := *messages.NewBaseMessage(a.Floor())
-	a.SendMessage(1, msg)
-	a.Log("Team 6 sent message:", infra.Fields{"floor": a.Floor(), "messageType": msg.MessageType()})
+	// msg := *messages.NewBaseMessage(a.Floor())
+	// a.SendMessage(1, msg)
+	// a.Log("Team 6 sent message:", infra.Fields{"floor": a.Floor(), "messageType": msg.MessageType()})
 
 }
--- pkg/agents/team6/strategies.go
+++ pkg/agents/team6/strategies.go
@@ -1,26 +1,89 @@
 package team6
 
-import "github.com/SOMAS2021/SOMAS2021/pkg/utils/globalTypes/food"
+import (
+	"math"
+	"math/rand"
+
+	"github.com/SOMAS2021/SOMAS2021/pkg/utils/globalTypes/food"
+	"github.com/SOMAS2021/SOMAS2021/pkg/utils/globalTypes/health"
+)
 
 type thresholdData struct {
-	satisficeThresh food.FoodType
-	satisfyThresh   food.FoodType
-	maxIntake       food.FoodType
+	maxIntake food.FoodType
+}
+
+type levelsData struct { // tiers of HP
+	strongLevel  float64
+	healthyLevel float64
+	weakLevel    float64
+	critLevel    float64
 }
 
 func (a *CustomAgent6) foodIntake() food.FoodType {
-	thresholds := thresholdData{satisficeThresh: food.FoodType(20), satisfyThresh: food.FoodType(60), maxIntake: food.FoodType(80)}
+	healthInfo := a.HealthInfo()
+
+	thresholds := thresholdData{
+		maxIntake: food.FoodType(80),
+	}
+
+	levels := levelsData{
+		strongLevel:  0.6 * float64(healthInfo.MaxHP),
+		healthyLevel: 0.3 * float64(healthInfo.MaxHP),
+		weakLevel:    0.1 * float64(healthInfo.MaxHP),
+		critLevel:    0.0,
+	}
+
+	currentHP := float64(a.HP())
 
 	switch a.currBehaviour.String() {
-	case "Altruist":
+	case "Altruist": // Never eat
 		return food.FoodType(0)
-	case "Collectivist":
-		return thresholds.satisficeThresh
-	case "Selfish":
-		return thresholds.satisfyThresh
-	case "Narcissist":
+
+	case "Collectivist": // Only eat when in critical zone randomly before expiry
+		switch {
+		case currentHP >= levels.weakLevel:
+			a.foodTakeDay = rand.Intn(healthInfo.MaxDayCritical) // Stagger the days when agents return to weak
+			return food.FoodType(0)
+		case currentHP >= levels.critLevel:
+			if a.DaysAtCritical() == a.foodTakeDay {
+				return food.FoodType(healthInfo.HPReqCToW)
+			}
+			return food.FoodType(0)
+		default:
+			return food.FoodType(0)
+		}
+
+	case "Selfish": // Stay in Healthy zone
+		switch {
+		case currentHP >= levels.strongLevel:
+			return food.FoodType(0)
+		case currentHP >= levels.healthyLevel:
+			return foodRequired(currentHP, currentHP, healthInfo)
+		default:
+			return foodRequired(currentHP, levels.healthyLevel, healthInfo)
+		}
+
+	case "Narcissist": // Eat max intake (Possible TODO: Stay in strong instead?)
 		return thresholds.maxIntake
+
 	default:
 		return food.FoodType(0)
 	}
 }
+
+func foodRequired(currentHP, goalHP float64, healthInfo *health.HealthInfo) food.FoodType {
+	denom := healthInfo.Width - goalHP + 0.75*currentHP - 10 + 0.25*float64(healthInfo.WeakLevel)
+	return food.FoodType(healthInfo.Tau * math.Log(healthInfo.Width/denom))
+}
+
+// func foodRequiredToStay(currentHP float64, currentLevel float64, levelWidth float64, tau float64) float64 {
+// 	return tau*math.Log(currentLevel+levelWidth-currentHP) - tau*math.Log(math.Pow(math.E, -1)*levelWidth)
+// }
+
+// func foodRequiredToAscend(currentHP float64, currentLevel float64, levelWidth float64, tau float64) float64 {
+// 	return tau*math.Log(currentLevel+levelWidth-currentHP) - tau*math.Log(math.Pow(math.E, -3)*levelWidth)
+// }
+
+// func hpFunc(x float64, currentHP float64, currentLevel float64, levelWidth float64, tau float64) float64 {
+// 	return currentLevel + levelWidth - math.Exp(-x/tau)*(levelWidth+currentHP-currentLevel)
+// }
