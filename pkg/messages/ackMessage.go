25218609c94f87e632ec033a2c12d527ebda0921
Nasim M <37720652+NasimMM@users.noreply.github.com>
2021-12-28 16:40:25 +0000

    commented ackMessages for a while

--- pkg/messages/ackMessage.go
+++ pkg/messages/ackMessage.go
@@ -1,18 +1,18 @@
 package messages
 
-type AckMessage struct {
-	*baseMessage
-	Ack bool
-}
+// type AckMessage struct {
+// 	*baseMessage
+// 	Ack bool
+// }
 
-func NewAckMessage(SenderFloor int, ack bool) *AckMessage {
-	msg := &AckMessage{
-		baseMessage: NewBaseMessage(SenderFloor),
-		Ack:         ack,
-	}
-	return msg
-}
+// func NewAckMessage(SenderFloor int, ack bool, messageType MessageType) *AckMessage {
+// 	msg := &AckMessage{
+// 		baseMessage: NewBaseMessage(SenderFloor, messageType),
+// 		Ack:         ack,
+// 	}
+// 	return msg
+// }
 
-func (msg AckMessage) MessageType() string {
-	return "AckMessage"
-}
+// func (msg AckMessage) MessageType() string {
+// 	return "AckMessage"
+// }
