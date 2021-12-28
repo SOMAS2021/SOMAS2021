28385ce6bfda70c9c4a86b7e3b1fc016542077a5
JaafarRammal <jarammal@gmail.com>
2021-12-28 18:34:35 +0000

    Add dummy frontend Add flags to main Add docker
    Co-authored-by: Udai Arneja <udai.arneja11ua@gmail.com>

---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"embed"
+	"flag"
+	"fmt"
+	"io/fs"
+	"math/rand"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
+
+	"github.com/SOMAS2021/SOMAS2021/pkg/config"
+	"github.com/SOMAS2021/SOMAS2021/pkg/simulation"
+	"github.com/SOMAS2021/SOMAS2021/pkg/utils/globalTypes/health"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	// processing the command-line flags
+	configPathPtr := flag.String("configpath", "config.json", "path for parameter configuration json file")
+	modePtr := flag.String("mode", "sim", "Execution mode. Either 'sim' for running a simulation and exiting, or 'serve' to serve a simulation endpoint")
+	portPtr := flag.Int("port", 9000, "Port to run the server on if mode='serve'")
+	flag.Parse()
+
+	// check backend mode
+	if *modePtr == "serve" {
+		port := ":" + strconv.Itoa(*portPtr)
+		fs := http.FileServer(http.Dir("./build"))
+		http.Handle("/", fs)
+		log.Println("Listening on " + port + "...")
+		http.HandleFunc("/simulate", func(w http.ResponseWriter, r *http.Request) {
+			// TODO add simulation end-point handling and parsing parameters from body json
+			fmt.Println("Will add simulation endpoint soon!")
+		})
+		err := http.ListenAndServe(port, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		fmt.Println("Loading parameters...")
+		// load parameters
+		parameters, err := config.LoadParamFromJson(*configPathPtr)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		runNewSimulation(parameters)
+	}
+}
+
+func runNewSimulation(parameters config.ConfigParameters) {
+	rand.Seed(time.Now().UnixNano())
+	f := setupLogFile()
+	healthInfo := health.NewHealthInfo(&parameters)
+
+	// TODO: agentParameters - struct
+	simEnv := simulation.NewSimEnv(&parameters, healthInfo)
+	simEnv.Simulate()
+	f.Close()
+}
+
+func getFileSystem(embeddedFiles embed.FS) http.FileSystem {
+	// Get the build subdirectory as the
+	// root directory so that it can be passed
+	// to the http.FileServer
+	fsys, err := fs.Sub(embeddedFiles, "build")
+	if err != nil {
+		fmt.Println("failed to start server: ", err)
+		return nil
+	}
+	return http.FS(fsys)
+}
+
+func setupLogFile() (fp *os.File) {
+	// Setup a log file
+	if _, err := os.Stat("logs"); os.IsNotExist(err) {
+		err := os.Mkdir("logs", 0755)
+		if err != nil {
+			fmt.Println("failed to create logs directory: ", err)
+			return
+		}
+	}
+
+	logfileName := filepath.Join("logs", time.Now().Format("2006-01-02-15-04-05")+".json")
+	f, err := os.OpenFile(logfileName, os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		fmt.Println("error opening file: ", err)
+		return
+	}
+	log.SetOutput(f)
+	log.SetFormatter(&log.JSONFormatter{})
+	return f
+}
