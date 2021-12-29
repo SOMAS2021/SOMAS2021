0d6171b610e810fa87e11eb78b10e04712c81af5
moinbukhari <56733483+moinbukhari@users.noreply.github.com>
2021-12-29 11:52:04 +0000

    Update pkg/agents/team7/agent1/agent.go
    Co-authored-by: Jason Zheng <jzz18@ic.ac.uk>

--- pkg/agents/team7/agent1/agent.go
+++ pkg/agents/team7/agent1/agent.go
@@ -157,29 +157,3 @@ func (a *CustomAgent7) Run() {
 
 }
 
-// type CustomAgent1 struct {
-// 	*infra.Base
-// 	// new params
-// }
-
-// func New(baseAgent *infra.Base) (infra.Agent, error) {
-// 	//create other parameters
-// 	return &CustomAgent1{
-// 		Base: baseAgent,
-// 	}, nil
-// }
-
-// func (a *CustomAgent1) Run() {
-// 	a.Log("Team7Agent1 reporting status:", infra.Fields{"floor": a.Floor(), "hp": a.HP()})
-
-// 	//UserID := a.ID()
-// 	currentHP := a.HP()
-// 	//currentFloor := a.Floor()
-// 	//currentAvailFood := a.CurrPlatFood()
-
-// 	var foodtotake food.FoodType = food.FoodType(100 - currentHP)
-// 	if foodtotake != 0 {
-// 		a.TakeFood(foodtotake)
-// 	}
-
-// }
