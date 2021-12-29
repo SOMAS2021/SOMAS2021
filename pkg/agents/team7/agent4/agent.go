4479cb5dae58a18d480ab8b703539500ff695ede
Nasim M <37720652+NasimMM@users.noreply.github.com>
2021-12-29 08:07:18 +0000

    temp agent 4 zaid and nasims ver

--- pkg/agents/team7/agent4/agent.go
+++ pkg/agents/team7/agent4/agent.go
@@ -7,16 +7,21 @@ import (
 type CustomAgent1 struct {
 	*infra.Base
 	// new params
-	prevFloors      []int
-	daysElapsed     int
-	trustOfAgents   map[string]int
-	agentTrustLevel int
+	prevFloors        map[int]int
+	orderPrevFloors   []int
+	currentDayonFloor int
+	reshuffle         int
+	trustOfAgents     map[string]int
+	agentTrustLevel   int
+	lastHP            int
 }
 
 func New(baseAgent *infra.Base) (infra.Agent, error) {
 	//create other parameters
 	return &CustomAgent1{
-		Base: baseAgent,
+		Base:              baseAgent,
+		currentDayonFloor: 1,
+		lastHP:            100,
 	}, nil
 }
 
@@ -24,10 +29,34 @@ func (a *CustomAgent1) Run() {
 	a.Log("Team 7 Agent 4 reporting status:", infra.Fields{"floor": a.Floor(), "hp": a.HP()})
 
 	// UserID := a.ID()
-	// currentHP := a.HP()
-	// currentAvailFood := a.CurrPlatFood()
+	currentHP := a.HP()
+	currentAvailFood := a.CurrPlatFood()
+
+	if currentHP < a.lastHP {
+		a.lastHP = currentHP
+		a.currentDayonFloor++
+	}
 
 	currentFloor := a.Floor()
-	a.prevFloors = append(a.prevFloors, currentFloor)
+	// if(a.prevFloors[len(a.prevFloors) - 1] != currentFloor){
+	// 	a.prevFloors = append(a.prevFloors, currentFloor)
+	// }
+	if a.orderPrevFloors[len(a.orderPrevFloors)-1] != currentFloor {
+		a.orderPrevFloors = append(a.orderPrevFloors, currentFloor)
+	}
+
+	if a.currentDayonFloor == 0 {
+
+	}
+
+	//BUG: if on reshuffle end up on same floor code breaks, reshuffle day calculation wrong, average wrong
+	if a.prevFloors[currentFloor] == 0 {
+		a.prevFloors[currentFloor] = int(currentAvailFood)
+	} else {
+		temp := (a.currentDayonFloor - 1) * a.prevFloors[currentFloor]
+		temp += int(currentAvailFood)
+		temp = temp / a.currentDayonFloor
+		a.prevFloors[currentFloor] = temp
+	}
 
 }
