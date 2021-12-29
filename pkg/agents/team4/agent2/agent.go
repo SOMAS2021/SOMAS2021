bc0fc463e94f16e17cb0cf61f4699e6b6c12df9e
Jason Keung <jk4818@ic.ac.uk>
2021-12-29 11:56:32 +0000

    feat: added message handlers

--- pkg/agents/team4/agent2/agent.go
+++ pkg/agents/team4/agent2/agent.go
@@ -9,76 +9,113 @@ import (
 type CustomAgent4 struct {
 	*infra.Base
 	myNumber int
-	globalTrust int
+	globalTrust float32
+	globalTrustAdd float32
+	globalTrustSubtract float32
+	coefficients []float32
+	lastFoodTaken int
+	IntendedFoodTaken int 
 }
 
 func New(baseAgent *infra.Base) (agent.Agent, error) {
 	return &CustomAgent4{
 		Base:     baseAgent,
-		myNumber: 0,
+		
 		globalTrust: 0.0, // MAKE SURE TO AMEND FIGURES FOR SENSIBLE AGENT BEHAVIOUR
-		globalTicks: 0,
-		globalTrustAdd: 9,
-		globalTrustSubtract: -9,
-		coefficients: [],
+		globalTrustAdd: 9.0,
+		globalTrustSubtract: -9.0,
+		coefficients: [0.1],
+		
+		lastFoodTaken: 0,
+		IntendedFoodTaken: 0
 	}, nil
 }
 
 func (a *CustomAgent4) Run() {
 	a.Log("Reporting agent state", infra.Fields{"health": a.HP(), "floor": a.Floor()})
 
-	defer a.globalTicks++ // count number of passed ticks/ days passed
-	if globalTicks %2 ==0 && globalTicks !=0{
-		receivedMsg := a.Base.ReceiveMessage()
-		switch receivedMsg.MessageType() {
-		case "AckMessage":
-			a.globalTrust += a.globalTrustAdd  *  0.1// TODO AND WORK ON
-		// case "foodOnPlatMessage":
-		// 	if receivedMsg.food == a.CurrPlatFood() && a.CurrPlatFood() != -1
-	  case "LeaveFoodMessage":
-			if receivedMsg.food == a.currPlatFood() && receivedMsg.senderFloor - a.Floor() == -1 && a.CurrPlatFood() != -1{ // on the floor above you
-				a.globalTrust+= a.globalTrustAdd //
-				if a.globalTrust > 100.0{
-					a.globalTrust = 100.0
-				}
+		// receivedMsg := a.Base.ReceiveMessage()
+		// switch receivedMsg.MessageType() {
+		// case "AckMessage":
+		// 	a.globalTrust += a.globalTrustAdd  *  coefficients[0]// TODO AND WORK ON
+		// 	if a.globalTrust > 100.0{
+		// 		a.globalTrust = 100.0
+		// 	}
+		// // case "foodOnPlatMessage":
+		// // 	if receivedMsg.food == a.CurrPlatFood() && a.CurrPlatFood() != -1
+	 	//  case "LeaveFoodMessage":
+		// 	if receivedMsg.food == a.currPlatFood() && receivedMsg.senderFloor - a.Floor() == -1 && a.CurrPlatFood() != -1{ // on the floor above you
+		// 		a.globalTrust+= a.globalTrustAdd //
+		// 		if a.globalTrust > 100.0{
+		// 			a.globalTrust = 100.0
+		// 		}
 
-			} else if receivedMsg.food != a.currPlatFood() && receivedMsg.senderFloor -a.Floor() == 1 && a.CurrPlatFood() != -1{ // on the floor below you
+		// 	} else if receivedMsg.food != a.currPlatFood() && receivedMsg.senderFloor -a.Floor() == 1 && a.CurrPlatFood() != -1{ // on the floor below you
 
-			}
-
-
-		default:
-
-		}
+		// 	}
 
 
+		// default:
 
-	}
-
-	foodtake_amount = int(a.currPlatFood() * (1- a.globalTrust/100))
-
-	if foodtake_amount !=0{
-		a.TakeFood(int(a.currPlatFood() * (1- a.globalTrust/100)))
-	}
-
-	// switch receivedMsg.MessageType() {
-	// case condition:
-	//
-	// }
+		// }
+	receivedMsg := a.ReceiveMessage()
 	if receivedMsg != nil {
-		a.Log("I sent a message", infra.Fields{"message": receivedMsg.MessageType()})
+		receivedMsg.Visit(a)
 	} else {
 		a.Log("I got nothing")
 	}
 
-	if (a.myNumber)%2 == 0 {
-		msg := *messages.NewAckMessage(int(a.Floor()), true)
-		a.SendMessage(1, msg)
-		a.Log("I sent a message", infra.Fields{"message": msg.MessageType()})
-	} else {
-		msg := *messages.NewBaseMessage(int(a.Floor()))
-		a.SendMessage(1, msg)
-	}
-	a.Log("My agent is doing something", infra.Fields{"thing": "potatoe", "another_thing": "another potatoe"})
-	a.TakeFood(10)
+	a.IntendedFoodTaken = food.FoodType(a.currPlatFood() * (1- a.globalTrust/100))
+	a.lastFoodTaken = a.TakeFood(a.IntendedFoodTaken)
+	
+}
+
+func (a *CustomAgent2) HandleAskHP(msg messages.AskHPMessage) {
+	reply := msg.Reply(a.Floor(), a.HP())
+	a.SendMessage(msg.SenderFloor()-a.Floor(), reply)
+	a.Log("I received an askHP message from ", infra.Fields{"floor": msg.SenderFloor(), "hp": reply.hp}})
+}
+
+func (a *CustomAgent2) HandleAskFoodTaken(msg messages.AskFoodTakenMessage) {
+	reply := msg.Reply(a.Floor(), a.lastFoodTaken)
+	a.SendMessage(msg.SenderFloor()-a.Floor(), reply)
+	a.Log("I received an askFoodTaken message from ", infra.Fields{"floor": msg.SenderFloor(), "food": reply.food})
+}
+
+func (a *CustomAgent2) HandleAskIntendedFoodTaken(msg messages.AskIntendedFoodIntakeMessage) {
+	reply := msg.Reply(a.Floor(), a.IntendedFoodTaken)
+	a.SendMessage(msg.SenderFloor()-a.Floor(), reply)
+	a.Log("I received an askIntendedFoodTaken message from ", infra.Fields{"floor": msg.SenderFloor(), "food": reply.food}})
+}
+
+func (a *CustomAgent2) HandleRequestLeaveFood(msg messages.RequestLeaveFoodMessage) {
+	reply := msg.Reply(a.Floor(), true) // Change for later dependent on circumstance
+	a.SendMessage(msg.SenderFloor()-a.Floor(), reply)
+	a.Log("I received a requestLeaveFood message from ", infra.Fields{"floor": msg.SenderFloor()}})
+}
+
+func (a *CustomAgent2) HandleRequestTakeFood(msg messages.RequestTakeFoodMessage) {
+	reply := msg.Reply(a.Floor(), true) // Change for later dependent on circumstance
+	a.SendMessage(msg.SenderFloor()-a.Floor(), reply)
+	a.Log("I received a requestTakeFood message from ", infra.Fields{"floor": msg.SenderFloor()}})
+}
+
+func (a *CustomAgent2) HandleResponse(msg messages.BoolResponseMessage) {
+	response := msg.Response() // Change for later dependent on circumstance
+	a.Log("I received a Response message from ", infra.Fields{"floor": msg.SenderFloor(), "response": response})
+}
+
+func (a *CustomAgent2) HandleStateFoodTaken(msg messages.StateFoodTakenMessage) {
+	statement := msg.Statement()
+	a.Log("I received a StateFoodTaken message from ", infra.Fields{"floor": msg.SenderFloor(), "food": statement}})
+}
+
+func (a *CustomAgent2) HandleStateHP(msg messages.StateHPMessage) {
+	statement := msg.Statement()
+	a.Log("I received a StateHP message from ", infra.Fields{"floor": msg.SenderFloor(), "hp": statement})
+}
+
+func (a *CustomAgent2) HandleStateIntendedFoodTaken(msg messages.StateIntendedFoodIntakeMessage) {
+	statement := msg.Statement()
+	a.Log("I received a StateIntendedFoodTaken message from ", infra.Fields{"floor": msg.SenderFloor(), "food": statement})
 }
