e1cd3a9c2251b4dcc4c3cf4e001b435ec71d1db4
hussain2603 <56412306+hussain2603@users.noreply.github.com>
2021-12-30 16:44:26 +0400

    Add UUIDs to messages (#140)
    * unique message IDs
    
    * changes to team1, 4, 6
    
    * changed back to main
    
    * fix: fixed fmt issue
    
    * Fixed Agent Responses two work with 2 params
    
    * removed agent2
    
    * removed agent2
    
    * fix: fmted responseMessage.go
    
    * added case 5 different message
    
    * Fixed so need of a default but the case still runs
    
    * changed returnId to requestId
    
    Co-authored-by: Jason Keung <jk4818@ic.ac.uk>
    Co-authored-by: JeetendraJoshi <jeetendrajoshi23@gmail.com>

--- pkg/messages/responseMessage.go
+++ pkg/messages/responseMessage.go
@@ -4,19 +4,25 @@ import "github.com/google/uuid"
 
 type BoolResponseMessage struct {
 	*BaseMessage
-	response bool
+	response  bool
+	requestId uuid.UUID
 }
 
-func NewResponseMessage(senderID uuid.UUID, senderFloor int, response bool) *BoolResponseMessage {
+func NewResponseMessage(senderID uuid.UUID, senderFloor int, response bool, requestId uuid.UUID) *BoolResponseMessage {
 	msg := &BoolResponseMessage{
 		NewBaseMessage(senderID, senderFloor, Response),
 		response,
+		requestId,
 	}
 	return msg
 }
 
 func (msg *BoolResponseMessage) Response() bool {
 	return msg.response
+}
+
+func (msg *BoolResponseMessage) RequestId() uuid.UUID {
+	return msg.requestId
 }
 
 func (msg *BoolResponseMessage) Visit(a Agent) {
