0ffa4be75f9f42fbb43e0f91797d440eaafdd9b2
Jaafar Rammal <45538723+JaafarRammal@users.noreply.github.com>
2021-12-30 12:54:05 +0000

    Dashboard skeleton (#164)
    * remove init
    
    * sidebar
    
    * Add blueprint cuz it's better + navbar
    
    * add bootstrap and results examples
    
    * looks nicer
    
    * prettier + add new run skeleton
    
    * new run in bottom right and can use background
    
    * prettier config
    
    * add toaster for notifs
    
    * minor fixes
    
    * Color results
    
    * improve menu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import NewRun from "./Components/NewRun";
+import Results from "./Components/Results";
+import Sidebar from "./Components/Sidebar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <div style={{ height: 50 }}></div>
+      <div className="row">
+        <div className="col-lg-2">
+          <Sidebar />
+        </div>
+        <div className="col-lg-10">
+          <Results />
+        </div>
+      </div>
+      <NewRun />
     </div>
   );
 }
