d6c3c0ac3989ab1822f3361f21ddca053a38f953
hussain2603 <56412306+hussain2603@users.noreply.github.com>
2021-12-30 16:44:26 +0400

    Add UUIDs to messages (#140)
    * unique message IDs
    
    * changes to team1, 4, 6
    
    * changed back to main
    
    * fix: fixed fmt issue
    
    * Fixed Agent Responses two work with 2 params
    
    * removed agent2
    
    * removed agent2
    
    * fix: fmted responseMessage.go
    
    * added case 5 different message
    
    * Fixed so need of a default but the case still runs
    
    * changed returnId to requestId
    
    Co-authored-by: Jason Keung <jk4818@ic.ac.uk>
    Co-authored-by: JeetendraJoshi <jeetendrajoshi23@gmail.com>

--- pkg/messages/baseMessage.go
+++ pkg/messages/baseMessage.go
@@ -41,6 +41,7 @@ type Agent interface {
 type Message interface {
 	MessageType() MessageType
 	SenderFloor() int
+	ID() uuid.UUID
 	Visit(a Agent)
 }
 
@@ -69,12 +70,14 @@ type ProposalMessage interface {
 type ResponseMessage interface {
 	Message
 	Response() bool
+	RequestId() uuid.UUID
 }
 
 type BaseMessage struct {
 	senderID    uuid.UUID
 	senderFloor int
 	messageType MessageType
+	id          uuid.UUID
 }
 
 func NewBaseMessage(senderID uuid.UUID, senderFloor int, messageType MessageType) *BaseMessage {
@@ -82,6 +85,7 @@ func NewBaseMessage(senderID uuid.UUID, senderFloor int, messageType MessageType
 		senderID:    senderID,
 		senderFloor: senderFloor,
 		messageType: messageType,
+		id:          uuid.New(),
 	}
 	return msg
 }
@@ -94,6 +98,9 @@ func (msg BaseMessage) SenderFloor() int {
 	return msg.senderFloor
 }
 
+func (msg BaseMessage) ID() uuid.UUID {
+	return msg.id
+}
 func (msg BaseMessage) SenderID() uuid.UUID {
 	return msg.senderID
 }
