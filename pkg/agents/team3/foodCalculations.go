3af0bd4625d37aa165a96738564b7b5e12877a35
Eschamp01 <edwardjls@gmail.com>
2021-12-31 16:01:37 +0100

    Update food calculations
    -Mood no longer used, HP now used
    -Cases for different daysAtCritical
    -Helper functions included for expandability

--- pkg/agents/team3/foodCalculations.go
+++ pkg/agents/team3/foodCalculations.go
@@ -6,53 +6,89 @@ import (
 
 //To Do:
 //1. Remove int() from platFood once platFood is type int
-//2. Change last resort critical state to "if a.HP() == 1 && a.daysInCriticalState == 2" once daysInCriticalState is implemented
-//3. Maybe improve foodCalc depending on the performance of the current version (S curve maybe). Provide multiple versions for different cases?
+
+func foodReqCalc(a *CustomAgent3, targetHP int) int {
+	decayCurrentHP := int((float64(targetHP+a.HealthInfo().HPLossBase)-float64(a.HealthInfo().WeakLevel)*a.HealthInfo().HPLossSlope)/(1.0-a.HealthInfo().HPLossSlope) + 1)
+	return int(-a.HealthInfo().Tau * math.Log(1.0-(float64(decayCurrentHP-a.HP())/a.HealthInfo().Width)))
+}
+
+func targetHPCalc(a *CustomAgent3) int { //10hp -> target is 20hp, 65hp -> target is 65hp, 100hp -> target is 93hp
+	return int(12.0 + 9.0*float64(a.HP())/11.0)
+}
+
+func foodRange(metric int, min int, max int) int {
+	if max > min && max >= 0 && min >= 0 {
+		return int(float64(max) - math.Round((float64(metric) / (100.0 / float64(max-min)))))
+	}
+	return -1
+}
+
+func foodScale(number int, metric int, minScale float64, maxScale float64) int {
+	if maxScale > minScale && maxScale >= 0 && minScale >= 0 {
+		return int(float64(number)*minScale*float64(metric)/100.0 + float64(number)*maxScale*float64(100-metric)/100.0)
+	}
+	return -1
+}
 
 func takeFoodCalculation(a *CustomAgent3) int {
 
 	platFood := int(a.CurrPlatFood())
+	hpCtoW := a.HealthInfo().HPReqCToW
+	morality := a.vars.morality
+	foodToEat := a.decisions.foodToEat
+	foodToLeave := a.decisions.foodToLeave
 
-	switch a.decisions.foodToEat | a.decisions.foodToLeave {
-	case (-1 | -1): //uses platFood, HP and morality
+	switch foodToEat | foodToLeave {
 
-		//TO DO: Modify for the new HP functions and requirements
-		if a.HP() == 1 { //if critical, calculates to take minimum 2 food if possible. Full range = 2-10
-			return int(10.0 - math.Floor((float64(a.vars.morality) / 25.0)) - math.Floor((float64(a.vars.mood))/25.0))
+	case (-1 | -1): // uses HP and morality
+
+		switch hp := a.HP(); {
+		case hp == 5:
+			if a.DaysAtCritical() == 1 { //depend on morality
+				return foodRange(morality, 0, hpCtoW+1) //range 0 to hpCtoW+1
+			} else if a.DaysAtCritical() == 2 {
+				return foodRange(morality, 1, hpCtoW+2) //range 1 to hpCtoW+2
+			} else { // a.DaysAtCritical() == 3
+				return foodRange(morality, hpCtoW, hpCtoW+3) //range hpCtoW to hpCtoW+3
+			}
+		default: // 10 <= hp && hp <= 100:
+			targetHP := targetHPCalc(a)
+			foodRequired := foodReqCalc(a, targetHP)
+			return foodScale(foodRequired, morality, 0.5, 1.5) // from foodRequired*0.5 (morality 100) to foodRequired*1.5 (morality 0)
 		}
 
-		foodCalc := int(10.0 - math.Floor((float64(a.vars.morality) / 20.0)) - math.Floor((float64(a.vars.mood))/20.0))
-
-		if platFood >= foodCalc {
-			return foodCalc
-		} else {
-			return platFood //if platfood smaller than foodCalc eat all thats left
+	case (-1 | foodToLeave): //uses platFood, HP, morality and foodToLeave
+		switch hp := a.HP(); {
+		case hp == 5:
+			if a.DaysAtCritical() == 1 { //depend on morality
+				foodToEat = foodRange(morality, 0, hpCtoW+1) //range 0 to hpCtoW+1
+			} else if a.DaysAtCritical() == 2 {
+				foodToEat = foodRange(morality, 1, hpCtoW+2) //range 1 to hpCtoW+2
+			} else { // a.DaysAtCritical() == 3
+				foodToEat = foodRange(morality, hpCtoW, hpCtoW+3) //range hpCtoW to hpCtoW+3
+			}
+		default: // 10 <= hp && hp <= 100:
+			targetHP := targetHPCalc(a)
+			foodRequired := foodReqCalc(a, targetHP)
+			foodToEat = foodScale(foodRequired, morality, 0.5, 1.5) // from foodRequired*0.5 (morality 100) to foodRequired*1.5 (morality 0)
 		}
 
-	case (-1 | a.decisions.foodToLeave): //uses platFood, HP, morality and foodToLeave
+		if platFood > foodToLeave {
+			return foodToEat
+		} else if platFood-foodToLeave > 0 {
+			return platFood - foodToLeave
+		} // case platFood - a.decisions.foodToLeave < 0
+		return 0
 
-		if a.HP() == 1 { //if critical, calculates to take minimum 2 food if possible. Full range = 2-10
-			return int(10.0 - math.Floor((float64(a.vars.morality) / 25.0)) - math.Floor((float64(a.vars.mood))/25.0))
-		}
-
-		foodCalc := int(10.0 - math.Floor((float64(a.vars.morality) / 20.0)) - math.Floor((float64(a.vars.mood))/20.0)) //adjust to latest values
-
-		if platFood-a.decisions.foodToLeave >= foodCalc {
-			return foodCalc
-		} else if platFood-a.decisions.foodToLeave >= 0 { //e.g. platFood=15, foodToLeave=10, foodCalc=10 --> if morality=100, take 5, if morality=0, take 10
-			return int(math.Floor(float64(platFood-a.decisions.foodToLeave)*float64(a.vars.morality)/100.0 + float64(foodCalc)*(100.0-float64(a.vars.morality))/100.0))
-		} //e.g. platFood=300, foodToLeave=310, foodCalc=10
-		return int(math.Floor(float64(foodCalc) * (100.0 - float64(a.vars.morality)) / 100.0))
-
-	case (a.decisions.foodToEat | -1): //uses foodToEat
-		return a.decisions.foodToEat
+	case (foodToEat | -1): //uses foodToEat
+		return foodToEat
 
 	default: //uses foodToEat and foodToLeave
-		if platFood-a.decisions.foodToLeave >= a.decisions.foodToEat {
-			return a.decisions.foodToEat
-		} else if platFood-a.decisions.foodToLeave >= 0 { //e.g. platFood=15, foodToLeave=10, foodCalc=10 --> if morality=100, take 5, if morality=0, take 10
-			return int(math.Floor(float64(platFood-a.decisions.foodToLeave)*float64(a.vars.morality)/100.0 + float64(a.decisions.foodToEat)*(100.0-float64(a.vars.morality))/100.0))
-		} //e.g. platFood=300, foodToLeave=310, foodCalc=10
-		return int(math.Floor(float64(a.decisions.foodToEat) * (100.0 - float64(a.vars.morality)) / 100.0))
+		if platFood >= foodToEat+foodToLeave {
+			return foodToEat
+		} else if platFood >= foodToLeave { //e.g. platFood=15, foodToLeave=10, foodToEat=10 --> if morality=100, take 5, if morality=0, take 10
+			return foodRange(morality, platFood-foodToLeave, foodToEat)
+		} //e.g. platFood=300, foodToLeave=310, foodToEat=10, take less if moral since you "should" leave a high amount
+		return foodRange(morality, 0, foodToEat)
 	}
 }
