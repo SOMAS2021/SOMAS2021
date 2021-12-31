85019fa1269f84d92a24d669c9d3bef1491b7638
Jason Keung <jk4818@ic.ac.uk>
2021-12-31 12:16:47 +0000

    feat: implemented more handlers and cleaned up switch case statement

--- pkg/agents/team4/agent2/agent.go
+++ pkg/agents/team4/agent2/agent.go
@@ -5,8 +5,6 @@ import (
 
 	"fmt"
 
-	"fmt"
-
 	"github.com/SOMAS2021/SOMAS2021/pkg/infra"
 	"github.com/SOMAS2021/SOMAS2021/pkg/messages"
 	"github.com/SOMAS2021/SOMAS2021/pkg/utils/globalTypes/food"
