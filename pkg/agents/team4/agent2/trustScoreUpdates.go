2dd6442f3cad4a43096fd3b4a00b282934a15b92
hussain2603 <56412306+hussain2603@users.noreply.github.com>
2022-01-02 22:00:42 +0400

    simplified msg mem

--- pkg/agents/team4/agent2/trustScoreUpdates.go
+++ pkg/agents/team4/agent2/trustScoreUpdates.go
@@ -9,24 +9,23 @@ import (
 )
 
 func (a *CustomAgent4) CheckForResponse(msg messages.BoolResponseMessage) {
-	if a.PlatformOnFloor() && len(a.responseMessages.messages) > 0 { // Check if there are any responses messages.
-		for i := 0; i < len(a.responseMessages.messages); i++ { // Iterate through each response message
-			respMsg := a.responseMessages.messages[i]
+	if a.PlatformOnFloor() && len(a.responseMessages) > 0 { // Check if there are any responses messages.
+		for i := 0; i < len(a.responseMessages); i++ { // Iterate through each response message
+			respMsg := a.responseMessages[i]
 			resMsg, ok := respMsg.(messages.ResponseMessage)
 			if !ok {
 				err := fmt.Errorf("ResponseMessage type assertion failed")
 				fmt.Println(err.Error())
 			} else {
-				for j := 0; j < len(a.sentMessages.messages); j++ { // Iterate through each sent message
-					sentMsg := a.sentMessages.messages[j]
-					sentMsgDir := a.sentMessages.direction[j]
+				for j := 0; j < len(a.sentMessages); j++ { // Iterate through each sent message
+					sentMsg := a.sentMessages[j]
 
 					if resMsg.RequestID() == sentMsg.ID() { // Find the corresponding response message that's been sent
-						a.sentMessages.messages, a.sentMessages.direction = remove(a.sentMessages, j) // Remove the accessed response/sent messages from memory
-						a.responseMessages.messages, a.responseMessages.direction = remove(a.responseMessages, i)
+						a.sentMessages = remove(a.sentMessages, j) // Remove the accessed response/sent messages from memory
+						a.responseMessages = remove(a.responseMessages, i)
 						a.Log("Team4 received a message", infra.Fields{"sender_uuid": msg.ID(), "sentmessage_uuid": sentMsg.ID()})
 
-						if sentMsg.MessageType() == messages.RequestLeaveFood && sentMsgDir == 1 { //TODO: theres now target floors and not directions anymore
+						if sentMsg.MessageType() == messages.RequestLeaveFood && a.Floor()-msg.SenderFloor() == 1 { //TODO: theres now target floors and not directions anymore
 							a.Log("Team4 reponse message received", infra.Fields{"sentMsg_Type": sentMsg.MessageType()})
 							reqMsg, ok := sentMsg.(messages.RequestMessage)
 							if !ok {
@@ -47,23 +46,22 @@ func (a *CustomAgent4) CheckForResponse(msg messages.BoolResponseMessage) {
 
 		}
 	} else if a.lastFoodTaken+a.CurrPlatFood() != a.lastPlatFood {
-		for i := 0; i < len(a.responseMessages.messages); i++ { // Iterate through each response message
-			respMsg := a.responseMessages.messages[i]
+		for i := 0; i < len(a.responseMessages); i++ { // Iterate through each response message
+			respMsg := a.responseMessages[i]
 			resMsg, ok := respMsg.(messages.ResponseMessage)
 			if !ok {
 				err := fmt.Errorf("ResponseMessage type assertion failed")
 				fmt.Println(err.Error())
 			} else {
-				for j := 0; j < len(a.sentMessages.messages); j++ { // Iterate through each sent message
-					sentMsg := a.sentMessages.messages[j]
-					sentMsgDir := a.sentMessages.direction[j]
+				for j := 0; j < len(a.sentMessages); j++ { // Iterate through each sent message
+					sentMsg := a.sentMessages[j]
 
 					if resMsg.RequestID() == sentMsg.ID() { // Find the corresponding response message that's been sent
-						a.sentMessages.messages, a.sentMessages.direction = remove(a.sentMessages, j) // Remove the accessed response/sent messages from memory
-						a.responseMessages.messages, a.responseMessages.direction = remove(a.responseMessages, i)
+						a.sentMessages = remove(a.sentMessages, j) // Remove the accessed response/sent messages from memory
+						a.responseMessages = remove(a.responseMessages, i)
 						a.Log("Team4 received a message", infra.Fields{"sender_uuid": msg.ID(), "sentmessage_uuid": sentMsg.ID()})
 
-						if sentMsg.MessageType() == messages.RequestTakeFood && sentMsgDir == -1 {
+						if sentMsg.MessageType() == messages.RequestTakeFood && a.Floor()-msg.SenderFloor() == -1 {
 							reqMsg, ok := sentMsg.(messages.RequestMessage)
 							if !ok {
 								err := fmt.Errorf("RequestMessage type assertion failed")
@@ -83,15 +81,15 @@ func (a *CustomAgent4) CheckForResponse(msg messages.BoolResponseMessage) {
 		}
 
 	} else {
-		for j := 0; j < len(a.sentMessages.messages); j++ {
-			if msg.RequestID() == a.sentMessages.messages[j].ID() {
-				sentMsg := a.sentMessages.messages[j]
+		for j := 0; j < len(a.sentMessages); j++ {
+			if msg.RequestID() == a.sentMessages[j].ID() {
+				sentMsg := a.sentMessages[j]
 				a.Log("Team4 received a message", infra.Fields{"sender_uuid": msg.ID(), "sentmessage_uuid": sentMsg.ID()})
 
 				if sentMsg.MessageType() == messages.RequestTakeFood && a.NeighbourFoodEaten() == -1 {
-					a.AppendToMessageMemory(a.Floor()-msg.SenderFloor(), &msg, a.responseMessages)
+					a.AppendToMessageMemory(&msg, a.responseMessages)
 				} else if sentMsg.MessageType() == messages.RequestLeaveFood && !a.PlatformOnFloor() {
-					a.AppendToMessageMemory(a.Floor()-msg.SenderFloor(), &msg, a.responseMessages)
+					a.AppendToMessageMemory(&msg, a.responseMessages)
 				} else if sentMsg.MessageType() == messages.RequestTakeFood && a.Floor()-msg.SenderFloor() == 1 {
 					a.AddToGlobalTrust(a.coefficients[1])
 				} else if sentMsg.MessageType() == messages.RequestLeaveFood && a.Floor()-msg.SenderFloor() == -1 {
