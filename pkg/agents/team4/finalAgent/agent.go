e175a9c2e68174fdab6107d7744099574237f6ef
r15hil <rishilapple@gmail.com>
2022-01-03 18:43:56 +0000

    Fixed locking issues

--- pkg/agents/team4/finalAgent/agent.go
+++ pkg/agents/team4/finalAgent/agent.go
@@ -133,6 +133,10 @@ func (a *CustomAgentEvo) Run() {
 	healthLevelSeparation := int(0.33 * float64(a.HealthInfo().MaxHP-a.HealthInfo().WeakLevel))
 
 	if a.HP() <= a.HealthInfo().WeakLevel { //critical
+		// if a.PlatformOnFloor() {
+		// 	fmt.Println("IMA DIE")
+		// 	fmt.Println(a.CurrPlatFood())
+		// }
 		a.params.healthStatus = 0
 		a.params.locked = false
 		// if dayPass {
@@ -148,11 +152,13 @@ func (a *CustomAgentEvo) Run() {
 			// }
 		} else if a.HP() <= a.HealthInfo().WeakLevel+2*healthLevelSeparation { //normal
 			a.params.healthStatus = 2
+			a.params.locked = true
 			// if dayPass {
 			// 	a.params.traumaScaleFactor = math.Max(0, a.params.traumaScaleFactor-0.02)
 			// }
 		} else { //strong
 			a.params.healthStatus = 3
+			a.params.locked = true
 			// if dayPass {
 			// 	a.params.traumaScaleFactor = math.Max(0, a.params.traumaScaleFactor-0.03)
 			// }
@@ -174,6 +180,11 @@ func (a *CustomAgentEvo) Run() {
 		calculatedAmountToEat = food.FoodType(a.params.traumaScaleFactor * float64(a.params.foodToEat[a.params.currentPersonality][a.params.healthStatus]))
 		foodEaten, err = a.TakeFood(food.FoodType(calculatedAmountToEat))
 		a.params.ageLastEaten = a.Age()
+		// if a.PlatformOnFloor() {
+		// 	fmt.Println("EATING")
+		// 	fmt.Println(calculatedAmountToEat)
+		// 	fmt.Println(foodEaten)
+		// }
 		if err != nil {
 			switch err.(type) {
 			case *infra.FloorError:
