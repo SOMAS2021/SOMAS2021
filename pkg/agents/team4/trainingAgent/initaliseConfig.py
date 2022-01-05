79352ba54ef768674cfabd382bf51d7ecd2f0600
TeDand <tejas-d@hotmail.com>
2022-01-05 15:43:13 +0000

    changed initialisations for team 4 training agents

--- pkg/agents/team4/trainingAgent/initaliseConfig.py
+++ pkg/agents/team4/trainingAgent/initaliseConfig.py
@@ -15,26 +15,33 @@ number_of_agents = int(number_of_best_agents)
 # Initialising all configuration files
 best_agents = []
 
-# agent config template
-agent_config = {
-    "FoodToEat": [],
-    "WaitProbability": [],
-}
+
 for i in range(number_of_agents):
+    # agent config template
+    agent_config = {
+        "FoodToEat": [],
+        "WaitProbability": [],
+    }
+
     # increase food to eat by increments of 20
     agent_config["FoodToEat"] = [(i % 5) * 20
                                  for _ in range(int(number_of_health_levels))]
     # set WaitProbability to a random number between 0 and 100
     agent_config["WaitProbability"] = [random.randint(0, 100)
-                                  for _ in range(int(number_of_health_levels))]
+                                       for _ in range(int(number_of_health_levels))]
     best_agents.append(agent_config)
 
-agent_config_str = json.dumps(best_agents[0], indent=4) # convert to JSON string 
-agent_config_file.write(agent_config_str) # write to agentConfig.json file
+for best_agent in best_agents:
+    best_agent["FoodToEat"][0] = -1
+    best_agent["WaitProbability"][0] = -1
+
+agent_config_str = json.dumps(
+    best_agents[0], indent=4)  # convert to JSON string
+agent_config_file.write(agent_config_str)  # write to agentConfig.json file
 # print("Initialising agent config to: {0}".format(agent_config_str))
 
-best_agents_str = json.dumps(best_agents, indent=4) # convert to JSON string
-best_agents_file.write(best_agents_str) 
+best_agents_str = json.dumps(best_agents, indent=4)  # convert to JSON string
+best_agents_file.write(best_agents_str)
 # print("Initialising best agents to: {0}".format(best_agents_str))
 
 agent_config_file.close()
