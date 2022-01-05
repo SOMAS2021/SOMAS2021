debd469d27b76072dc73c49ee235f3e29565eca3
daryllimyt <daryllimyt@gmail.com>
2022-01-05 18:06:06 +0000

    add min and max functions for stats

--- frontend/src/Helpers/Utils.ts
+++ frontend/src/Helpers/Utils.ts
@@ -1,3 +1,9 @@
 export function Average(arr: number[]): number {
   return arr.reduce((a, b) => a + b) / arr.length;
 }
+export function Max(arr: number[]): number {
+  return arr.reduce((a, b) => Math.max(a, b));
+}
+export function Min(arr: number[]): number {
+  return arr.reduce((a, b) => Math.min(a, b));
+}
