db2ff1526ca26e1e6b5b3b3bef3572af85d8ef64
JaafarRammal <jarammal@gmail.com>
2022-01-05 23:27:31 +0000

    properly using .env for DEV

--- frontend/src/Helpers/API.ts
+++ frontend/src/Helpers/API.ts
@@ -6,7 +6,7 @@ import { Result } from "./Result";
 import { GetSimConfig, SimConfig } from "./SimConfig";
 
 function endpoint(req: string) {
-  return (process.env.DEV ? "http://localhost:9000/" : window.location) + req;
+  return (process.env.REACT_APP_DEV ? "http://localhost:9000/" : window.location) + req;
 }
 
 function parseResponse(res: any, key: string) {
