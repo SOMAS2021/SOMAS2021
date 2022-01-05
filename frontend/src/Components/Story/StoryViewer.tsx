0abaed1b060a64ee1f604cbb863ee363527e084b
JaafarRammal <jarammal@gmail.com>
2022-01-05 13:22:48 +0000

    make scrollable

--- frontend/src/Components/Story/StoryViewer.tsx
+++ frontend/src/Components/Story/StoryViewer.tsx
@@ -44,10 +44,10 @@ function StoryController(props: StoryControllerProps) {
   const maxTick = Math.max(...story.map((e) => e.tick));
 
   return (
-    <div>
+    <div style={{overflow: "hidden"}}>
       <Ticker tick={tick} setTick={setTick} maxTick={maxTick} />
       <Divider />
-      <div>
+      <div style={{height: "45vh", overflowY: "scroll"}}>
         {story.map((log, index) => {
           if (log.tick === tick) {
             switch (log.msg) {
