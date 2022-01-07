5fd5db6d098aad85afc72aec442990ce47f97e15
Tom Eaton <tomeaton17@gmail.com>
2022-01-07 15:07:39 +0100

    Added function to export Q Table to CSV file

--- pkg/agents/team2/qtable.go
+++ pkg/agents/team2/qtable.go
@@ -2,6 +2,10 @@ package team2
 
 import (
 	"math"
+	"encoding/csv"
+	"os"
+	"log"
+	"fmt"
 )
 
 type maxQAction struct {
@@ -35,4 +39,33 @@ func (a *CustomAgent2) updateQTable(state int, action int) {
 	Alpha := 0.1
 	a.qTable[state][action] = (1-Alpha)*a.qTable[state][action] +
 		Alpha*(a.rTable[state][action]+Gamma*a.getMaxQ(a.CheckState()).maxQ)
+}
+
+func (a *CustomAgent2) exportQTable() {
+	os.Remove("qtable.csv")
+	f, err := os.OpenFile("qtable.csv", os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	w := csv.NewWriter(f)
+	qTable := a.qTable
+	sQTable := make([][]string, len(qTable))
+	for i := 0; i < len(qTable); i++ {
+		sQTable[i] = make([]string, len(qTable[0]))
+	}
+	//w.WriteAll(a.qTable)
+
+	for i := 0; i < len(qTable); i++ {
+		for j := 0; j < len(qTable[0]); j++ {
+			sQTable[i][j] = fmt.Sprint(qTable[i][j])
+		}
+	}
+	fmt.Println("rows:", len(sQTable), "columns:", len(sQTable[0]))
+	w.WriteAll(sQTable)
+
+	if err := w.Error(); err != nil {
+		log.Fatalln("error writing csv:", err)
+	}
 }
