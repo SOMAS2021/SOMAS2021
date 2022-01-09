4f6b4d1655efa946d88f628724858f27674501fe
crsren <Chris.renschler@gmail.com>
2022-01-09 21:56:21 +0000

    Adding loss aversion

--- pkg/agents/team6/treaties.go
+++ pkg/agents/team6/treaties.go
@@ -20,12 +20,20 @@ func calculateUtility(x, z float64, socialMotive string) float64 {
 	params := newUtilityParams(socialMotive)
 	// Calculates the function scaling parameter a
 
+	var result float64
+
 	if socialMotive == "Altruist" || socialMotive == "Nacissist" {
 		// Don't scale depending on desired food
-		return params.g*math.Pow(x, 1/params.r) - params.c*x
+		result =  params.g*math.Pow(x, 1/params.r) - params.c*x
 	} else {
 		a := (1 / z) * math.Pow((params.c*params.r)/params.g, params.r/(1-params.r))
-		return params.g*math.Pow(a*x, 1/params.r) - params.c*a*x
+		result = params.g*math.Pow(a*x, 1/params.r) - params.c*a*x
+	}
+
+	if result < 0.0 {
+		return 1.2*result
+	} else {
+		return result
 	}
 }
 
