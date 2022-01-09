5c724cf50c19bf3f32591cde223b86a0708e8639
JaafarRammal <jarammal@gmail.com>
2022-01-09 17:08:39 +0000

    fix graph for food

--- frontend/src/Helpers/Logging/Food.ts
+++ frontend/src/Helpers/Logging/Food.ts
@@ -2,24 +2,29 @@ import { GetFile } from "../API";
 import { Log } from "./Log";
 
 export interface FoodLog extends Log {
-  food: number
+  food: number;
 }
 
 export function GetFoodLogs(filename: string): Promise<FoodLog[]> {
   return new Promise<FoodLog[]>((resolve, reject) => {
     GetFile(filename, "food")
-      .then((food) =>
-        resolve(
-          food.map(function (e: any) {
-            const f: FoodLog = {
-              food: e["food"],
-              tick: e["tick"],
-              day: e["day"],
-            };
-            return f;
-          })
-        )
-      )
+      .then((food) => {
+        const foods: FoodLog[] = food.map(function (e: any) {
+          const f: FoodLog = {
+            food: e["food"],
+            tick: e["tick"],
+            day: e["day"],
+          };
+          return f;
+        });
+        var maxTick: number = foods[foods.length - 1].tick;
+        for (var i = 0; i < maxTick; i++) {
+          if (!foods[i] || foods[i].tick !== i + 1) {
+            foods.splice(i, 0, { tick: i + 1, day: foods[i - 1].day, food: foods[i - 1].food });
+          }
+        }
+        resolve(foods);
+      })
       .catch((err) => reject(err));
   });
 }
