9def93a2782b17fd49b737f8e9e080ccc667ca81
hussain2603 <56412306+hussain2603@users.noreply.github.com>
2022-01-09 21:37:05 +0400

    [Team 4] Update agent (#272)
    * Changed parameters being tuned in agent 4 evolutionary strategy
    
    * changed agent 4 evolution agent intialisation of food to eat values at critical HP
    
    * removed locked wait
    
    * added death rates
    
    * storing past agents params
    
    * fixed logging
    
    * pushing for combining with message passng
    
    * team4 agent mvp
    
    * created new trust score agent
    
    * Added Global Trust + Plat Food Messages
    
    * added take food parameter dependent on global trust
    
    * Added extra coefficients that can be trained using evolutionary algorithm
    
    * feat: added message handlers
    
    * FEAT: Added Handling and Setting up message passing
    
    * Added UUID's to msgs and extra msg handling
    
    * commented and fixed
    
    * agent2
    
    * bug: sentMessage.Request undefined
    
    * fix: latest code
    
    * Request() works now
    
    * fix: fixed message type request issue
    
    * feat: implemented more handlers and cleaned up switch case statement
    
    * feat: implemented more handlers and cleaned up switch case statement
    
    * Rebase hopefully done successfully?
    
    * team4 agent mvp
    
    * created new trust score agent
    
    * Added Global Trust + Plat Food Messages
    
    * feat: added message handlers
    
    * FEAT: Added Handling and Setting up message passing
    
    * Added UUID's to msgs and extra msg handling
    
    * fix: latest code
    
    * feat: implemented more handlers and cleaned up switch case statement
    
    * feat: implemented more handlers and cleaned up switch case statement
    
    * bug: BoolMsg not of message type
    
    * added messages.go
    
    * added trust_score_updates
    
    * code cleanup
    
    * fixed seg fault
    
    * simplified msg mem
    
    * added some comments
    
    * combined team4-trust and team4-evo
    
    * testing
    
    * added locking
    
    * rebased
    
    * Pushed logs
    
    * checked agent laerning
    
    * started to merge trust score and evo agent code
    
    * fixed boolean in python script to run selfessly without flag
    
    * Changing capitalisation of true and false in bash script
    
    * changed check for flag in python file
    
    * Separated team4 agents into training and final
    
    * Added final agent implementation
    
    * ignored the training logs and changed to final agent for team4
    
    * Fixed locking issues
    
    * change back to main values
    
    * applied suggested changes
    
    * Added neutral agent
    
    * Added mutation mechanisms and probability generation
    
    * feat: Major revamp on generateNewBest Agents
    
    * before changing to probability to wait
    
    * changed to probabilities to wait
    
    * changed initialisations for team 4 training agents
    
    * Changed initialisation of team 4 training agents
    
    * fix: fixed days passed
    
    * updated configs
    
    * Changed team 4 training agent to latest version
    
    * fix: repaired rebase errors
    
    * feat: overhauled health boundings
    
    * Capped foodToEat values to 60 for team4
    
    * fix: changed agent being tested
    
    * added initial treaty implementation
    
    * Changed message passing implementation
    
    * added intended
    
    * updated finalAgent with treaty
    
    * feat: added initial agent update on treaty conditions
    
    * fixed: checking if no response
    
    * feat: MASSIVE OVERHAUL ULTIMATE AGENT
    
    * fix: changed Coefficients to trustCoefficients
    
    * feat: Updated training agent to latest
    
    * back to main settings
    
    * feat: added additional floors for messages to be sent
    
    * applied changes from suggestions
    
    Co-authored-by: TeDand <tejas-d@hotmail.com>
    Co-authored-by: r15hil <rishilapple@gmail.com>
    Co-authored-by: Jason Keung <jasonkeung@Jasons-MacBook-Pro.local>
    Co-authored-by: JeetendraJoshi <jeetendrajoshi23@gmail.com>
    Co-authored-by: Jason Keung <jk4818@ic.ac.uk>

--- pkg/agents/team4/finalAgent/messages.go
+++ pkg/agents/team4/finalAgent/messages.go
@@ -6,76 +6,72 @@ import (
 	"github.com/SOMAS2021/SOMAS2021/pkg/utils/globalTypes/food"
 )
 
-func (a *CustomAgentEvo) SendingMessage() {
+// ---------- RECEIVE MESSAGE ---------//
 
-	var msg messages.Message
-	floorToSend := a.Floor() + 1
-	switch a.params.messageCounter {
-	case 0:
-		msg = messages.NewAskFoodTakenMessage(a.ID(), a.Floor(), floorToSend)
-		a.SendMessage(msg)
-		a.params.sentMessages = append(a.params.sentMessages, msg)
-		a.Log("Team4 agent sent a message", infra.Fields{"message": msg.MessageType().String()})
-	case 1:
-		msg = messages.NewAskHPMessage(a.ID(), a.Floor(), floorToSend)
-		a.SendMessage(msg)
-		a.params.sentMessages = append(a.params.sentMessages, msg)
-		a.Log("Team4 agent sent a message", infra.Fields{"message": msg.MessageType().String()})
-	case 2:
-		msg = messages.NewAskIntendedFoodIntakeMessage(a.ID(), a.Floor(), floorToSend)
-		a.SendMessage(msg)
-		a.params.sentMessages = append(a.params.sentMessages, msg)
-		a.Log("Team4 agent sent a message", infra.Fields{"message": msg.MessageType().String()})
-	case 3:
-		msg = messages.NewRequestLeaveFoodMessage(a.ID(), a.Floor(), floorToSend, 10) //need to change how much to request to leave
-		a.SendMessage(msg)
-		a.params.sentMessages = append(a.params.sentMessages, msg)
-		a.Log("Team4 agent sent a message", infra.Fields{"message": msg.MessageType().String()})
-	case 4:
-		msg = messages.NewRequestTakeFoodMessage(a.ID(), a.Floor(), floorToSend, 20) //need to change how much to request to take
-		a.SendMessage(msg)
-		a.params.sentMessages = append(a.params.sentMessages, msg)
-		a.Log("Team4 agent sent a message", infra.Fields{"message": msg.MessageType().String()})
-	case 5:
-		floorToSend = a.Floor() - 1
-		msg = messages.NewAskFoodTakenMessage(a.ID(), a.Floor(), floorToSend)
-		a.SendMessage(msg)
-		a.params.sentMessages = append(a.params.sentMessages, msg)
-		a.Log("Team4 agent sent a message", infra.Fields{"message": msg.MessageType().String()})
-	case 6:
-		floorToSend = a.Floor() - 1
-		msg = messages.NewAskHPMessage(a.ID(), a.Floor(), floorToSend)
-		a.SendMessage(msg)
-		a.params.sentMessages = append(a.params.sentMessages, msg)
-		a.Log("Team4 agent sent a message", infra.Fields{"message": msg.MessageType().String()})
-	case 7:
-		floorToSend = a.Floor() - 1
-		msg = messages.NewAskIntendedFoodIntakeMessage(a.ID(), a.Floor(), floorToSend)
-		a.SendMessage(msg)
-		a.params.sentMessages = append(a.params.sentMessages, msg)
-		a.Log("Team4 agent sent a message", infra.Fields{"message": msg.MessageType().String()})
-	case 8:
-		floorToSend = a.Floor() - 1
-		msg = messages.NewRequestLeaveFoodMessage(a.ID(), a.Floor(), floorToSend, 10) //need to change how much to request to leave
-		a.SendMessage(msg)
-		a.params.sentMessages = append(a.params.sentMessages, msg)
-		a.Log("Team4 agent sent a message", infra.Fields{"message": msg.MessageType().String()})
-	case 9:
-		floorToSend = a.Floor() - 1
-		msg = messages.NewRequestTakeFoodMessage(a.ID(), a.Floor(), floorToSend, 20) //need to change how much to request to take
-		a.SendMessage(msg)
-		a.params.sentMessages = append(a.params.sentMessages, msg)
-		a.Log("Team4 agent sent a message", infra.Fields{"message": msg.MessageType().String()})
-	default:
-	}
+func (a *CustomAgentEvo) getMessage() {
+	receivedMsg := a.ReceiveMessage()
 
-	if a.HasDayPassed() {
-		a.params.messageCounter = 0
+	if receivedMsg != nil {
+		if receivedMsg.MessageType() == messages.RequestLeaveFood {
+			a.params.requestLeaveFoodMessages = append(a.params.requestLeaveFoodMessages, receivedMsg)
+		} else {
+			a.params.otherMessageBuffer = append(a.params.otherMessageBuffer, receivedMsg)
+		}
+		a.params.receivedMessagesCount++
 	}
-	a.params.messageCounter++
 }
 
-//FOR HONEST AGENTS
+// ----------- CALL THE RELEVANT MESSAGE HANDLER ---------//
+
+func (a *CustomAgentEvo) callHandleMessage() {
+	if a.PlatformOnFloor() && len(a.params.requestLeaveFoodMessages) > 0 {
+		a.params.requestLeaveFoodMessages[0].Visit(a)
+		a.params.requestLeaveFoodMessages = remove(a.params.requestLeaveFoodMessages, 0)
+	} else if len(a.params.otherMessageBuffer) > 0 {
+		a.params.otherMessageBuffer[0].Visit(a)
+		a.params.otherMessageBuffer = remove(a.params.otherMessageBuffer, 0)
+	} else {
+		a.Log("I got no messages")
+	}
+}
+
+/*------------------------HANDLING MESSAGES TO BE SENT ------------------------*/
+
+func (a *CustomAgentEvo) generateMessagesToSend() {
+	var msg messages.Message
+
+	// Request Agent above us to leave critical food amount when we have critical HP.
+	if a.params.healthStatus == 0 {
+		floorToSend := a.Floor() - 1
+		msg = messages.NewRequestLeaveFoodMessage(a.ID(), a.Floor(), floorToSend, a.params.foodToEat[a.params.currentPersonality][a.params.healthStatus])
+		a.params.msgToSendBuffer = append(a.params.msgToSendBuffer, msg)
+	}
+
+	directions := []int{-3, -2, -1, 1, 2, 3}
+	// Ask HP and Ask Intended Food Intake to figure out if the person is taking excessive food
+	for _, dir := range directions {
+		if dir+a.Floor() >= 1 {
+			msg = messages.NewAskHPMessage(a.ID(), a.Floor(), a.Floor()+dir) // send to the agent above
+			a.params.msgToSendBuffer = append(a.params.msgToSendBuffer, msg)
+			msg = messages.NewAskIntendedFoodIntakeMessage(a.ID(), a.Floor(), a.Floor()+dir) // send to the agent above
+			a.params.msgToSendBuffer = append(a.params.msgToSendBuffer, msg)
+		}
+	}
+}
+
+func (a *CustomAgentEvo) sendingMessage() {
+
+	if len(a.params.msgToSendBuffer) > 0 {
+		msg := a.params.msgToSendBuffer[0]
+		a.SendMessage(msg)
+		a.params.sentMessages = append(a.params.sentMessages, msg)
+		a.Log("Team4 agent sent a message", infra.Fields{"message": msg.MessageType()})
+		a.params.msgToSendBuffer = remove(a.params.msgToSendBuffer, 0)
+	}
+}
+
+/*------------------------HANDLING RECEIVED MESSAGES ------------------------*/
+
 func (a *CustomAgentEvo) HandleAskHP(msg messages.AskHPMessage) {
 	reply := msg.Reply(a.ID(), a.Floor(), msg.SenderFloor(), a.HP())
 	a.SendMessage(reply)
@@ -89,62 +85,67 @@ func (a *CustomAgentEvo) HandleAskFoodTaken(msg messages.AskFoodTakenMessage) {
 }
 
 func (a *CustomAgentEvo) HandleAskIntendedFoodTaken(msg messages.AskIntendedFoodIntakeMessage) {
-	reply := msg.Reply(a.ID(), a.Floor(), msg.SenderFloor(), int(a.params.foodToEat[a.params.currentPersonality][a.params.healthStatus]))
+	reply := msg.Reply(a.ID(), a.Floor(), msg.SenderFloor(), int(a.params.intendedFoodToTake))
 	a.SendMessage(reply)
-	a.Log("Team4 agent received an askIntendedFoodTaken message from ", infra.Fields{"floor": msg.SenderFloor(), "food": a.params.foodToEat[a.params.currentPersonality][a.params.healthStatus]})
+	a.Log("Team4 agent received an askIntendedFoodTaken message from ", infra.Fields{"floor": msg.SenderFloor(), "food": a.params.intendedFoodToTake})
 }
 
 func (a *CustomAgentEvo) HandleRequestLeaveFood(msg messages.RequestLeaveFoodMessage) {
-	response := true
-	if a.params.globalTrust < a.params.globalTrustLimit {
-		response = false
-	}
-	reply := msg.Reply(a.ID(), a.Floor(), msg.SenderFloor(), response) // TODO: Change for later dependent on circumstance
+	amount := msg.Request()
+	response := a.CurrPlatFood()-a.params.intendedFoodToTake >= food.FoodType(amount)
+	reply := msg.Reply(a.ID(), a.Floor(), msg.SenderFloor(), response)
 	a.SendMessage(reply)
 	a.Log("Team4 agent received a requestLeaveFood message from ", infra.Fields{"floor": msg.SenderFloor()})
 }
 
 func (a *CustomAgentEvo) HandleRequestTakeFood(msg messages.RequestTakeFoodMessage) {
 	amount := msg.Request()
-	response := a.params.foodToEat[a.params.currentPersonality][a.params.healthStatus] <= amount
-	reply := msg.Reply(a.ID(), a.Floor(), msg.SenderFloor(), response) // TODO: Change for later dependent on circumstance
+	response := a.params.intendedFoodToTake <= food.FoodType(amount)
+	reply := msg.Reply(a.ID(), a.Floor(), msg.SenderFloor(), response)
 	a.SendMessage(reply)
 	a.Log("Team4 agent received a requestTakeFood message from ", infra.Fields{"floor": msg.SenderFloor()})
 }
 
 func (a *CustomAgentEvo) HandleResponse(msg messages.BoolResponseMessage) {
-	response := msg.Response() // TODO: Change for later dependent on circumstance
+	response := msg.Response()
 	if !msg.Response() {
-		a.AddToGlobalTrust(-a.params.coefficients[1]) // TODO: adapt for other conditions
+		a.addToGlobalTrust(-a.params.trustCoefficients[1])
 	} else {
-		a.CheckForResponse(msg)
+		a.checkForResponse(msg)
 	}
 	a.Log("Team4 agent received a Response message from ", infra.Fields{"floor": msg.SenderFloor(), "response": response, "global_trust": a.params.globalTrust})
-
 }
 
 func (a *CustomAgentEvo) HandleStateFoodTaken(msg messages.StateFoodTakenMessage) {
 	statement := msg.Statement()
-	if food.FoodType(statement) > a.params.maxFoodLimit {
-		a.AddToGlobalTrust(-a.params.coefficients[1])
+	if food.FoodType(statement) > food.FoodType(a.params.foodToEat["selfish"][2]) {
+		a.addToGlobalTrust(-a.params.trustCoefficients[1])
 	} else {
-		a.AddToGlobalTrust(a.params.coefficients[1])
+		a.addToGlobalTrust(a.params.trustCoefficients[1])
 	}
 	a.Log("Team4 agent received a StateFoodTaken message from ", infra.Fields{"floor": msg.SenderFloor(), "food": statement, "global_trust": a.params.globalTrust})
 }
 
 func (a *CustomAgentEvo) HandleStateHP(msg messages.StateHPMessage) {
 	statement := msg.Statement()
-	a.AddToGlobalTrust(a.params.coefficients[0])
+
+	_, exists := a.params.agentResponseMemory[msg.SenderID()]
+	if !exists {
+		a.params.agentResponseMemory[msg.SenderID()] = []int{-1, -1} // initialise the map if sender not in map
+	}
+	a.params.agentResponseMemory[msg.SenderID()][0] = statement
+
 	a.Log("Team4 agent received a StateHP message from ", infra.Fields{"floor": msg.SenderFloor(), "hp": statement, "global_trust": a.params.globalTrust})
 }
 
 func (a *CustomAgentEvo) HandleStateIntendedFoodTaken(msg messages.StateIntendedFoodIntakeMessage) {
 	statement := msg.Statement()
-	if food.FoodType(statement) > a.params.maxFoodLimit {
-		a.AddToGlobalTrust(-a.params.coefficients[1])
-	} else {
-		a.AddToGlobalTrust(a.params.coefficients[1])
+
+	_, exists := a.params.agentResponseMemory[msg.SenderID()]
+	if !exists {
+		a.params.agentResponseMemory[msg.SenderID()] = []int{-1, -1} // initialise the map if sender not in map
 	}
+	a.params.agentResponseMemory[msg.SenderID()][1] = statement
+
 	a.Log("Team4 agent received a StateIntendedFoodTaken message from ", infra.Fields{"floor": msg.SenderFloor(), "food": statement, "global_trust": a.params.globalTrust})
 }
