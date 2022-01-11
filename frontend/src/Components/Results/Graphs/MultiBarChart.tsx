eb4f539aa0070d9e1e07051697b2408f3e81a702
daryllimyt <daryllimyt@gmail.com>
2022-01-11 02:33:03 +0000

    add multi bar chart

---
+++ frontend/src/Components/Results/Graphs/MultiBarChart.tsx
@@ -0,0 +1,35 @@
+import { Colors } from "@blueprintjs/core";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, BarElement, Title, Tooltip, Legend);
+
+interface MultiBarChartProps {
+  xAxis: any[];
+  data: any[];
+}
+// Y axis number of messages
+// X axis agent type
+// let data contain the values (y-axis) and titles (message type)
+// i.e. [{label1: [], data1: []}, ...]
+export default function MultiBarChart(props: MultiBarChartProps) {
+  const { xAxis, data } = props;
+  return (
+    <Bar
+      data={{
+        labels: xAxis,
+        datasets: data,
+      }}
+    />
+  );
+}
