67e6fe905f2e852a4f38a2c325323f8b6295ce50
Silvertrousers <c.goodarzi@gmail.com>
2022-01-15 13:51:43 +0000

    adding config genrator scripts and a way to run all of them

--- runAllConfigs.sh
+++ runAllConfigs.sh
@@ -1,9 +1,11 @@
 rm -r configs
 mkdir configs
 python3 configGen.py
+
 CONFIG_DIR="configs/*.json"
 for f in $CONFIG_DIR
 do
     echo "Running $f"
     go run main.go --configpath=$f
+    rm $f
 done
