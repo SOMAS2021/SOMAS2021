3ac3fc390c4c628ac20a1f3a38c543eb32646969
Silvertrousers <c.goodarzi@gmail.com>
2022-01-15 13:51:43 +0000

    adding config genrator scripts and a way to run all of them

--- configGen.py
+++ configGen.py
@@ -229,7 +229,6 @@ for foodPerAgent in [5]:
                     "HPLossSlope": {HPLossSlope},
                     "LogMain": {LogMain},
                     "LogStory": {LogStory},
-<<<<<<< HEAD
                     "SimTimeoutSeconds": {SimTimeoutSeconds},
                     "LogFileName": {logName}
                 """
@@ -373,17 +372,141 @@ for playerA in [2,3,4,5,6,7]:
         jsonString = "{" + jsonString + "}"
         f = open(f"configs/{configName}.json", "w")
         f.write(jsonString)
-<<<<<<< HEAD
         f.close()
-=======
+
+# Experiment 4
+print("Generating Experiment 4 Configs")
+for playerA in [2,3,4,5,6,7]:
+    for numSelfish in [0,1,2,3,4,5,6,7,8,9,10]:
+        
+        Tournament_array = [0,numSelfish,0,0,0,0,0,0]
+        Tournament_array[playerA] = 10
+        
+        UseFoodPerAgentRatio = "true"
+        FoodOnPlatform = 100 
+        FoodPerAgentRatio = foodPerAgent
+        RandomAgents = Tournament_array[0]
+        Team1Agents  = Tournament_array[1]
+        Team2Agents  = Tournament_array[2]
+        Team3Agents  = Tournament_array[3]
+        Team4Agents  = Tournament_array[4]
+        Team5Agents  = Tournament_array[5]
+        Team6Agents  = Tournament_array[6]
+        Team7Agents  = Tournament_array[7]
+        
+        TicksPerFloor = 10 
+        AgentHP = 100 
+        SimDays = 400
+        ReshuffleDays = 7
+        SimTimeoutSeconds = 1000
+
+        SimName = f"E4-selfish-{playerA}-{numSelfish}"
+        logName = f"log-{SimName}"
+        configName = f"config-{SimName}"
+
+        jsonString = f"""
+            "UseFoodPerAgentRatio": {UseFoodPerAgentRatio},
+            "FoodOnPlatform": {FoodOnPlatform},
+            "FoodPerAgentRatio": {FoodPerAgentRatio},
+            "Team1Agents": {Team1Agents},
+            "Team1Agents2": {Team1Agents2},
+            "Team2Agents": {Team2Agents},
+            "Team3Agents": {Team3Agents},
+            "Team4Agents": {Team4Agents},
+            "Team5Agents": {Team5Agents},
+            "Team6Agents": {Team6Agents},
+            "Team7Agents": {Team7Agents},
+            "RandomAgents": {RandomAgents},
+            "AgentHP": {AgentHP},
+            "AgentsPerFloor": {AgentsPerFloor},
+            "TicksPerFloor": {TicksPerFloor},
+            "SimDays": {SimDays},
+            "ReshuffleDays": {ReshuffleDays},
+            "maxHP": {maxHP},
+            "weakLevel": {weakLevel},
+            "width": {width},
+            "tau": {tau},
+            "MaxFoodIntake": {MaxFoodIntake},
+            "hpReqCToW": {hpReqCToW},
+            "hpCritical": {hpCritical},
+            "maxDayCritical": {maxDayCritical},
+            "HPLossBase": {HPLossBase},
+            "HPLossSlope": {HPLossSlope},
+            "LogMain": {LogMain},
+            "LogStory": {LogStory},
+            "SimTimeoutSeconds": {SimTimeoutSeconds},
+            "LogFileName": {logName}
+        """
+        
+        jsonString = "{" + jsonString + "}"
+        f = open(f"configs/{configName}.json", "w")
+        f.write(jsonString)
+        f.close()
+playerA = 0
+for playerA in [2,3,4,5,6,7]:
+    for numRand in [0,1,2,3,4,5,6,7,8,9,10]:
+        
+        Tournament_array = [numRand,0,0,0,0,0,0,0]
+        Tournament_array[playerA] = 10
+        
+        UseFoodPerAgentRatio = "true"
+        FoodOnPlatform = 100 
+        FoodPerAgentRatio = foodPerAgent
+        RandomAgents = Tournament_array[0]
+        Team1Agents  = Tournament_array[1]
+        Team2Agents  = Tournament_array[2]
+        Team3Agents  = Tournament_array[3]
+        Team4Agents  = Tournament_array[4]
+        Team5Agents  = Tournament_array[5]
+        Team6Agents  = Tournament_array[6]
+        Team7Agents  = Tournament_array[7]
+        
+        TicksPerFloor = 10 
+        AgentHP = 100 
+        SimDays = 400
+        ReshuffleDays = 7
+        SimTimeoutSeconds = 1000
+
+        SimName = f"E4-random-{playerB}-{numRand}"
+        logName = f"log-{SimName}"
+        configName = f"config-{SimName}"
+
+        jsonString = f"""
+            "UseFoodPerAgentRatio": {UseFoodPerAgentRatio},
+            "FoodOnPlatform": {FoodOnPlatform},
+            "FoodPerAgentRatio": {FoodPerAgentRatio},
+            "Team1Agents": {Team1Agents},
+            "Team1Agents2": {Team1Agents2},
+            "Team2Agents": {Team2Agents},
+            "Team3Agents": {Team3Agents},
+            "Team4Agents": {Team4Agents},
+            "Team5Agents": {Team5Agents},
+            "Team6Agents": {Team6Agents},
+            "Team7Agents": {Team7Agents},
+            "RandomAgents": {RandomAgents},
+            "AgentHP": {AgentHP},
+            "AgentsPerFloor": {AgentsPerFloor},
+            "TicksPerFloor": {TicksPerFloor},
+            "SimDays": {SimDays},
+            "ReshuffleDays": {ReshuffleDays},
+            "maxHP": {maxHP},
+            "weakLevel": {weakLevel},
+            "width": {width},
+            "tau": {tau},
+            "MaxFoodIntake": {MaxFoodIntake},
+            "hpReqCToW": {hpReqCToW},
+            "hpCritical": {hpCritical},
+            "maxDayCritical": {maxDayCritical},
+            "HPLossBase": {HPLossBase},
+            "HPLossSlope": {HPLossSlope},
+            "LogMain": {LogMain},
+            "LogStory": {LogStory},
+            "SimTimeoutSeconds": {SimTimeoutSeconds},
+            "LogFileName": {logName}
+        """
+        
+        jsonString = "{" + jsonString + "}"
+        f = open(f"configs/{configName}.json", "w")
+        f.write(jsonString)
         f.close()
 >>>>>>> adding config genrator scripts and a way to run all of them
-=======
-                    "SimTimeoutSeconds": {SimTimeoutSeconds}
-                """
-                
-                jsonString = "{" + jsonString + "}"
-                f = open(f"configs/configTournament-{playerA}-{playerB}-{FoodPerAgentRatio}.json", "w")
-                f.write(jsonString)
-                f.close()
->>>>>>> made come configs and added some scripts
